31dbbd696b69ea55d0d451e0eb68aae671ce3ce5
Kevin Lu <kevinlu1248@gmail.com>
2023-06-23 19:26:01 -0700

    done refactor

--- tests/test_tiktoken.py
+++ tests/test_tiktoken.py
@@ -1,6 +1,8 @@
 import modal
 
+from src.utils.constants import UTILS_NAME
+
 if __name__ == "__main__":
-    count = modal.Function.lookup("utils", "Tiktoken.count")
+    count = modal.Function.lookup(UTILS_NAME, "Tiktoken.count")
     print(count.call("Hello world!"))
 
