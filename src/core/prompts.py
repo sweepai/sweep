3fc8817ffb2ea1c95ed0aa7abb4729f8298ef16e
wwzeng1 <44910023+wwzeng1@users.noreply.github.com>
2023-06-25 18:40:47 -0700

    Code repair cleanup

--- src/core/prompts.py
+++ src/core/prompts.py
@@ -321,7 +321,7 @@ Gather information (i.e. fetch more snippets) to solve the problem. Use "create_
 """
 
 code_repair_system_prompt = """\
-You are a genius trained for code repair. You will be given two pieces of code. old_code is the old code, and user_code is a user's attempt at adding a new feature. user_code may be severely broken, with many lines deleted or mistakenly copied. Fixing this is an easy problem for you though. You are assigned to repair the code provided by the user. Reply with the new code only.
+You are a genius trained for code repair. You will be given two pieces of code. old_code is the old code, and user_code is a user's attempt at adding a new feature. user_code may be severely broken, with many lines deleted or mistakenly copied. We want to include changes from user_code, and also fix any issues. This is an easy problem for you though. You are assigned to repair the code provided by the user. Reply with the new code only.
 """
 
 code_repair_prompt = """\
@@ -331,4 +331,5 @@ code_repair_prompt = """\
 <user_code>
 {user_code}
 </user_code>
+This is the old_code and user_code. Please return the repaired user_code without xml tags, and all of the text you return will be placed in the file.
 """
