98159bf1c71dfe41bae3e5ad8deaa1ac6402146e
wwzeng1 <44910023+wwzeng1@users.noreply.github.com>
2023-06-27 23:32:32 -0700

    fix prompts

--- src/core/prompts.py
+++ src/core/prompts.py
@@ -323,7 +323,7 @@ Gather information (i.e. fetch more snippets) to solve the problem. Use "create_
 """
 
 code_repair_system_prompt = """\
-You are a genius trained for code repair. This entails fixing syntax errors and duplicated/missing lines, but not code cleanup/style changes. You will be given two pieces of code. old_code is the old code, and user_code is a user's attempt at adding a new feature. user_code may be severely broken, with many lines deleted or mistakenly copied. old_code should be working. We want to include changes from user_code, and also fix any issues using our knowledge of both the old_code and user_code files. This is an easy problem for you. You are assigned to repair the code provided by the user. Reply with the new code only.
+You are a genius trained for code repair. This entails fixing syntax errors and duplicated/missing lines, but not code cleanup/style changes. You will be given two pieces of code. old_code is the old code, and user_code is a user's attempt at adding a new feature.  We want to include all of the changes from user_code. We should also fix any issues using our knowledge of both the old_code and user_code files. This is an easy problem for you. You are assigned to repair the code provided by the user. Reply with the new code only.
 """
 
 code_repair_prompt = """\
@@ -333,5 +333,5 @@ code_repair_prompt = """\
 <user_code>
 {user_code}
 </user_code>
-This is the old_code and user_code. Please return the repaired user_code without xml tags, and all of the text you return will be placed in the file.
+This is the old_code and user_code. Please return the repaired user_code without xml tags, and all of the text you return will be placed in the file. Avoid changing, adding, or removing any logic in user_code.
 """
