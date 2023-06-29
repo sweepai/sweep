c8d7ffc8d17b352aa86f4af05d4590a98f748a63
Kevin Lu <kevinlu1248@gmail.com>
2023-06-28 19:28:48 -0700

    more minor changes

--- src/app/backend.py
+++ src/app/backend.py
@@ -7,8 +7,9 @@ from loguru import logger
 from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
-from src.core.entities import Snippet
 
+# from src.core.chat import ChatGPT
+from src.core.entities import Message, Snippet
 from src.utils.constants import BOT_TOKEN_NAME, DB_NAME, PREFIX
 from src.utils.github_utils import get_github_client
 
@@ -22,6 +23,7 @@ image = (
         "loguru",
         "tqdm",
         "posthog",
+        "openai",
         "highlight-io",
         "PyGithub",
         "GitPython",
@@ -46,7 +48,7 @@ def _asgi_app():
         query: str,
         n_results: int = 5,
         installation_id: int | None = None
-    ):
+    ) -> list[Snippet]:
         get_relevant_snippets = modal.Function.lookup(DB_NAME, "get_relevant_snippets")
         snippets: list[Snippet] = get_relevant_snippets.call(
             repo_name,
@@ -63,9 +65,23 @@ def _asgi_app():
                 logger.error(snippet)
         return snippets
     
-    @app.post("/chat")
-    def chat(messages: list[tuple[str, str]]):
-        pass
+    # @app.post("/chat")
+    # def chat(messages: list[tuple[str, str]]) -> str:
+    #     messages_for_openai: list[Message] = []
+    #     for message in messages:
+    #         if message[0] is None:
+    #             messages_for_openai.append(Message(
+    #                 role="assistant",
+    #                 content=message[0],
+    #             ))
+    #         else:
+    #             messages_for_openai.append(Message(
+    #                 role="user",
+    #                 content=message[1],
+    #             ))
+    #     chatgpt = ChatGPT()
+    #     result = chatgpt.chat(messages_for_openai)
+    #     return result
 
     return app
 
@@ -90,4 +106,13 @@ class APIClient(BaseModel):
         )
         snippets = [Snippet(**item) for item in results.json()]
         return snippets
+    
+    def chat(self, messages: list[tuple[str, str]]) -> str:
+        results = requests.post(
+            self.api_endpoint + "/chat",
+            params={
+                "messages": messages,
+            }
+        )
+        return results.text
     
