c3fb707e2af07825f02af29e0660a8f16ba9c0ac
Lucas Jagg <lukejagg@gmail.com>
2023-06-30 12:13:05 -0700

    Update prompts.py

--- src/core/prompts.py
+++ src/core/prompts.py
@@ -263,6 +263,7 @@ Generate a new_file based on the given plan, ensuring that you:
 2. Provide complete functions with actual business logic. It is imperative that we do not leave any work to the user/future readers of this code.
 3. Do not write new "todo" comments.
 4. Do not write incomplete functions or line numbers.
+5. Make sure code follows programming language conventions in repo
 
 Instead of writing "# Rest of Code", specify the lines to copy from the old file using an XML tag, inclusive (e.g., "<copied>0-25</copied>"). Make sure to use this exact format.
 Copy the correct line numbers and copy as long of a prefix and suffix as possible. For instance, if you want to insert code after line 50, start with "<copied>0-50</copied>".
