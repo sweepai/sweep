0e738b25c36476cd85eec5e4a7b1fd5da8b01a49
Kevin Lu <kevinlu1248@gmail.com>
2023-06-30 23:21:43 -0700

    some minor fixes

--- src/app/sweep.py
+++ src/app/sweep.py
@@ -1,23 +1,21 @@
 import typer
 from rich import print
 
-from src.app.config import Config
+from src.app.config import SweepChatConfig
 
 app = typer.Typer()
 
 @app.command()
+def install():
+    SweepChatConfig.load(recreate=True)
+    print("[green]Setup completed successfully![/green]")
+
+@app.command()
 def start():
+    SweepChatConfig.load()
     from src.app.ui import demo
     demo.queue()
     demo.launch(enable_queue=True, inbrowser=True)
-
-@app.command()
-def setup(recreate: bool = False):
-    if Config.is_initialized():
-        print("[green]Setup already completed![/green]")
-    else:
-        Config.load(recreate=recreate)
-        print("[green]Setup completed successfully![/green]")
 
 if __name__ == "__main__":
     app()
