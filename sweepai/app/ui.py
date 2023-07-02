4ae5d96695cf2fb4427c155918c9a6b0f04419a3
wwzeng1 <44910023+wwzeng1@users.noreply.github.com>
2023-07-01 22:34:12 -0700

    override

--- sweepai/app/ui.py
+++ sweepai/app/ui.py
@@ -42,9 +42,9 @@ with gr.Blocks(theme=gr.themes.Soft(), title="Sweep Chat", css="footer {visibili
     msg = gr.Textbox(label="Message to Sweep", placeholder="Write unit tests for OpenAI calls")
     clear = gr.ClearButton([msg, chatbot, snippets_text])
 
-    snippets: list[Snippet] = []
     proposed_pr: str | None = None
-    selected_files = []
+    searched = False
+    selected_snippets = []
     file_to_str = {}
 
     def repo_name_change(repo_full_name):
@@ -69,18 +69,22 @@ with gr.Blocks(theme=gr.themes.Soft(), title="Sweep Chat", css="footer {visibili
     repo_full_name.change(repo_name_change, [repo_full_name], [msg])
 
     def build_string():
-        global selected_files
+        global selected_snippets
         global file_to_str
-        for file_name in selected_files:
+        for snippet in selected_snippets:
+            file_name = snippet.file_path
             if file_name not in file_to_str:
                 add_file_to_dict(file_name)
-        snippets_text = "### Relevant snippets:\n" + "\n\n".join([file_to_str[fi] for fi in selected_files])
+        snippets_text = "### Relevant snippets:\n" + "\n\n".join([file_to_str[snippet.file_path] for snippet in selected_snippets])
         return snippets_text
 
     def add_file_to_dict(file_name):
         global file_to_str
         global path_to_contents
-        file_contents = path_to_contents[file_name]
+        if file_name in path_to_contents:
+            file_contents = path_to_contents[file_name]
+        else:
+            file_contents = repo.get_contents(file_name).decoded_content.decode('utf-8')
         file_contents_split = file_contents.split("\n")
         length = len(file_contents_split)
         backtick, escaped_backtick = "`", "\\`"
@@ -88,9 +92,10 @@ with gr.Blocks(theme=gr.themes.Soft(), title="Sweep Chat", css="footer {visibili
         file_to_str[file_name] = f'{file_name}:0:{length}\n```python\n{preview}\n...\n```'
     
     def file_names_change(file_names):
-        global selected_files
+        global selected_snippets
         global file_to_str
-        selected_files = file_names
+        global path_to_contents
+        selected_snippets = [Snippet(content=path_to_contents[file_name], start=0, end=path_to_contents[file_name].count('\n'), file_path=file_name) for file_name in file_names]
         return file_names, build_string()
     
     file_names.change(file_names_change, [file_names], [file_names, snippets_text])
@@ -100,30 +105,30 @@ with gr.Blocks(theme=gr.themes.Soft(), title="Sweep Chat", css="footer {visibili
             raise Exception("Set the repository name first")
         return gr.update(value="", interactive=False), history + [[user_message, None]]
 
-    def handle_message_stream(chat_history: list[tuple[str | None, str | None]], snippets_text, repo_name):
-        global selected_files
-        snippets = []
-        yield chat_history, snippets_text
-        if snippets_text and snippets_text.strip().count("\n") == 0:
+    def handle_message_stream(chat_history: list[tuple[str | None, str | None]], snippets_text, file_names):
+        global selected_snippets
+        global searched
+        yield chat_history, snippets_text, file_names
+        if snippets_text and not searched:
+            searched = True
             # Searching for relevant snippets
             chat_history[-1][1] = "Searching for relevant snippets..."
-            yield chat_history, snippets_text
+            snippets_text = build_string()
+            yield chat_history, snippets_text, file_names
             logger.info("Fetching relevant snippets...")
-            snippets = api_client.search(chat_history[-1][0], 3)
+            selected_snippets += api_client.search(chat_history[-1][0], 3)
+            snippets_text = build_string()
+            yield chat_history, snippets_text, file_names
             logger.info("Fetched relevant snippets.")
             chat_history[-1][1] = "Found relevant snippets."
-            
-            backtick, escaped_backtick = "`", "\\`"
             # Update using chat_history
-            files_snippets_text = []
-            snippets_text = "### Relevant snippets:\n" + "\n".join([f"{snippet.denotation}\n```python\n{snippet.get_preview(5).replace(backtick, escaped_backtick)}\n```" for snippet in snippets]
-                                                                   + files_snippets_text)
-            yield chat_history, snippets_text
+            snippets_text = build_string()
+            yield chat_history, snippets_text, file_names
         
         global proposed_pr
         if proposed_pr and chat_history[-1][0].strip().lower() in ("okay", "ok"):
             chat_history[-1][1] = f"⏳ Creating PR..."
-            yield chat_history, snippets_text
+            yield chat_history, snippets_text, file_names
             pull_request = api_client.create_pr(
                 file_change_requests=[(item["file_path"], item["instructions"]) for item in proposed_pr["plan"]],
                 pull_request={
@@ -134,29 +139,29 @@ with gr.Blocks(theme=gr.themes.Soft(), title="Sweep Chat", css="footer {visibili
                 messages=chat_history,
             )
             chat_history[-1][1] = f"✅ PR created at {pull_request['html_url']}"
-            yield chat_history, snippets_text
+            yield chat_history, snippets_text, file_names
             return
 
         # Generate response
         logger.info("...")
         chat_history.append([None, "Fetching endpoint..."])
-        yield chat_history, snippets_text
+        yield chat_history, snippets_text, file_names
         chat_history[-1][1] = ""
         logger.info("Starting to generate response...")
-        stream = api_client.stream_chat(chat_history, snippets)
+        stream = api_client.stream_chat(chat_history, selected_snippets)
         function_name = ""
         raw_arguments = ""
         for chunk in stream:
             if chunk.get("content"):
                 token = chunk["content"]
                 chat_history[-1][1] += token
-                yield chat_history, snippets_text
+                yield chat_history, snippets_text, file_names
             if chunk.get("function_call"):
                 function_call = chunk["function_call"]
                 function_name = function_name or function_call.get("name")
                 raw_arguments += function_call.get("arguments")
                 chat_history[-1][1] = f"Calling function: `{function_name}`\n```json\n{raw_arguments}\n```"
-                yield chat_history, snippets_text
+                yield chat_history, snippets_text, file_names
         if function_name:
             arguments = json.loads(raw_arguments)
             if function_name == "create_pr":
@@ -170,12 +175,12 @@ with gr.Blocks(theme=gr.themes.Soft(), title="Sweep Chat", css="footer {visibili
                     summary=arguments["summary"],
                     plan="\n".join([f"* `{item['file_path']}`: {item['instructions']}" for item in arguments["plan"]])
                 )
-                yield chat_history, snippets_text
+                yield chat_history, snippets_text, file_names
                 proposed_pr = arguments
             else:
                 raise NotImplementedError
 
-    response = msg.submit(handle_message_submit, [repo_full_name, msg, chatbot], [msg, chatbot], queue=False).then(handle_message_stream, [chatbot, snippets_text, repo_full_name], [chatbot, snippets_text])
+    response = msg.submit(handle_message_submit, [repo_full_name, msg, chatbot], [msg, chatbot], queue=False).then(handle_message_stream, [chatbot, snippets_text, file_names], [chatbot, snippets_text, file_names])
     response.then(lambda: gr.update(interactive=True), None, [msg], queue=False)
 
 
