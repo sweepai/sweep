c35b851f5652cc9fbe50da0fb1be72b3708315f0
asc11cat <asc11cat@protonmail.com>
2023-07-05 10:38:56 +0200

    fix(posthog): pass a mock key inst if key is not present

--- sweepai/utils/event_logger.py
+++ sweepai/utils/event_logger.py
@@ -6,7 +6,7 @@ from posthog import Posthog
 
 POSTHOG_API_KEY = os.environ.get("POSTHOG_API_KEY")
 if POSTHOG_API_KEY is None:
-    posthog = Posthog(disabled=True)
+    posthog = Posthog(project_api_key="none", disabled=True, host='https://app.posthog.com')
     logger.warning("Initialized an empty Posthog instance as POSTHOG_API_KEY is not present.")
 else:
     posthog = Posthog(project_api_key=POSTHOG_API_KEY, host='https://app.posthog.com')
