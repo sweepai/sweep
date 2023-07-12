a6f8e51fdedc7275ed3e1642fd6eb997342650b2
Lucas Jagg <lukejagg@gmail.com>
2023-07-11 22:44:25 -0700

    Sweep config

--- sweepai/utils/constants.py
+++ sweepai/utils/constants.py
@@ -28,7 +28,12 @@ LABEL_COLOR = "9400D3"
 LABEL_DESCRIPTION = "Sweep your software chores"
 
 SWEEP_CONFIG_BRANCH = "sweep/add-sweep-config"
-DEFAULT_CONFIG = """# Reference: https://github.com/sweepai/sweep/blob/main/.github/sweep.yaml. If you use this be sure to frequently sync your default branch(main, master) to dev.
-branch: '{branch}'"""
+DEFAULT_CONFIG = """# Sweep AI turns bug fixes & feature requests into code changes (https://sweep.dev)
+# For details on our config file, check out our docs at https://docs.sweep.dev
+# Reference: https://github.com/sweepai/sweep/blob/main/.github/sweep.yaml.
+
+# If you use this be sure to frequently sync your default branch(main, master) to dev.
+branch: '{branch}'
+"""
 
 SECONDARY_MODEL = "gpt-3.5-turbo-16k-0613"
