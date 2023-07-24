17bb17133161aa79cc26a7513e39a9c3e8bebc8a
Kevin Lu <kevinlu1248@gmail.com>
2023-07-23 23:13:27 -0700

    MVP for new GHA

--- sweepai/handlers/on_check_suite.py
+++ sweepai/handlers/on_check_suite.py
@@ -8,11 +8,15 @@ from loguru import logger
 
 from sweepai.core.gha_extraction import GHAExtractor
 from sweepai.events import CheckRunCompleted
+from sweepai.handlers.on_comment import on_comment
 from sweepai.utils.config.client import SweepConfig, get_gha_enabled
 from sweepai.utils.github_utils import get_github_client, get_token
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
+log_message = """GitHub actions yielded the following error:
+
+{error_logs}"""
 
 def download_logs(repo_full_name: str, run_id: int, installation_id: int):
     headers = {
@@ -56,13 +60,16 @@ def clean_logs(logs_str: str):
 
 
 def on_check_suite(request: CheckRunCompleted):
+    logger.info(f"Received check run completed event for {request.repository.full_name}")
     g = get_github_client(request.installation.id)
     repo = g.get_repo(request.repository.full_name)
     if not get_gha_enabled(repo):
+        logger.info(f"Skipping github action for {request.repository.full_name} because it is not enabled")
         return None
     pr = repo.get_pull(request.check_run.pull_requests[0].number)
     num_pr_commits = len(list(pr.get_commits()))
-    if num_pr_commits > 6:
+    if num_pr_commits > 20:
+        logger.info(f"Skipping github action for PR with {num_pr_commits} commits")
         return None
     logger.info(f"Running github action for PR with {num_pr_commits} commits")
     logs = download_logs(
@@ -75,5 +82,19 @@ def on_check_suite(request: CheckRunCompleted):
     logs = clean_logs(logs)
     extractor = GHAExtractor()
     logger.info(f"Extracting logs from {request.repository.full_name}, logs: {logs}")
-    logs = extractor.gha_extract(logs)
-    return logs
+    problematic_logs = extractor.gha_extract(logs)
+    comment = pr.as_issue().create_comment(problematic_logs.format(error_logs=logs))
+    logger.info(f"Summary of logs: {problematic_logs}")
+    on_comment(
+        repo_full_name=request.repository.full_name,
+        repo_description=request.repository.description,
+        comment="sweep: " + problematic_logs,
+        pr_path=None,
+        pr_line_position=None,
+        username=request.sender.login,
+        installation_id=request.installation.id,
+        pr_number=request.check_run.pull_requests[0].number,
+        comment_id=comment.id,
+    )
+    return {"success": True}
+
