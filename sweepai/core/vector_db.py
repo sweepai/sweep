f67327aea1fbc8fa709ffdaee46f3c664e7ece9a
wwzeng1 <44910023+wwzeng1@users.noreply.github.com>
2023-07-24 20:39:46 -0700

    fix scoring

--- sweepai/core/vector_db.py
+++ sweepai/core/vector_db.py
@@ -19,7 +19,7 @@ from sweepai.utils.hash import hash_sha256
 from sweepai.utils.scorer import compute_score, convert_to_percentiles
 from ..utils.config.client import SweepConfig
 from ..utils.config.server import ENV, DB_MODAL_INST_NAME, UTILS_MODAL_INST_NAME, REDIS_URL, BOT_TOKEN_NAME
-from ..utils.github_utils import get_file_age, get_token
+from ..utils.github_utils import get_token
 
 
 stub = modal.Stub(DB_MODAL_INST_NAME)
@@ -222,8 +222,7 @@ def get_deeplake_vs_from_repo(
                         scores.append(score)
                         continue
                 commits = list(repo.get_commits(path=file_path, sha=branch_name))
-                file_age_in_days = get_file_age(repo, file_path)
-                score = compute_score(contents, commits, file_age_in_days)
+                score = compute_score(contents, commits)
                 if cache_inst and cache_success:
                     cache_inst.set(cache_key, json.dumps(score), ex=60 * 60 * 2)
                 scores.append(score)
