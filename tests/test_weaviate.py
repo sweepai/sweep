e9bc02c2952748bffc1df57d2616651dba7eeaa6
wwzeng1 <44910023+wwzeng1@users.noreply.github.com>
2023-08-10 18:44:04 -0700

    update

--- tests/test_weaviate.py
+++ tests/test_weaviate.py
@@ -4,12 +4,12 @@ from sweepai.config.server import DOCS_MODAL_INST_NAME
 import modal 
 stub = modal.Stub(DOCS_MODAL_INST_NAME)
 # doc_url = "https://docs.anthropic.com/claude"
-doc_url = "https://pytorch.org/docs/stable/dynamo"
+doc_url = "https://particles.js.org/docs"
 update = modal.Function.lookup(DOCS_MODAL_INST_NAME, "daily_update")
 search = modal.Function.lookup(DOCS_MODAL_INST_NAME, "search_vector_store")
 write = modal.Function.lookup(DOCS_MODAL_INST_NAME, "write_documentation")
-# print(write.call(doc_url))
-results = search.call(doc_url, "torch.compile")
+print(write.call(doc_url))
+results = search.call(doc_url, "Where are the config files stored")
 metadatas = results["metadata"]
 docs = results["text"]
 print(docs[0])
