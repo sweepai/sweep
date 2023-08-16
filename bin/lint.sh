b397e0d1b4e2fed1149534014efb9b1c549f1489
Kevin Lu <kevinlu1248@gmail.com>
2023-08-15 23:47:39 -0700

    minor fixes

--- bin/lint.sh
+++ bin/lint.sh
@@ -1,2 +1,2 @@
-python_files=`git diff --name-only --diff-filter=d | grep -E '\.py$' | tr '\n' ' '`; \
+python_files=$(git diff --name-only --diff-filter=d --cached; git diff --name-only --diff-filter=d | grep -E '\.py$' | tr '\n' ' ')
 if [ -z "$python_files" ]; then true; else pylint $python_files --errors-only; fi
