909e5af7dc9303154192b00c24615abdb4a59976
Kevin Lu <kevinlu1248@gmail.com>
2023-08-16 20:20:37 -0700

    Fixed scorer

--- sweepai/handlers/on_ticket.py
+++ sweepai/handlers/on_ticket.py
@@ -415,9 +415,9 @@ async def on_ticket(
             -1,
         )
 
-    if (
-        repo_name != "sweep" and "sweep" in repo_name.lower()
-    ) or "test" in repo_name.lower():
+    if (repo_name != "sweep" and "sweep" in repo_name.lower()) or (
+        repo_name != "test-canary" and "test" in repo_name.lower()
+    ):
         # Todo(kevinlu1248): Instead of blocking, use faster model.
         edit_sweep_comment(
             "Sweep does not work on test repositories. Please create an issue on a real repository. If you think this is a mistake, please report this at https://discord.gg/sweep.",
