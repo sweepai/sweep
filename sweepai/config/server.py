b9f7121700f52169e499c68f7774f8870c3bd215
Kevin Lu <kevinlu1248@gmail.com>
2023-08-17 19:38:00 -0700

    Fixed dev

--- sweepai/config/server.py
+++ sweepai/config/server.py
@@ -1,6 +1,9 @@
 import os
 
+ENVIRONMENT = os.environ.get("ENVIRONMENT", "dev2")
+print(f"Using environment: {ENVIRONMENT}")
 PREFIX = "prod"
+
 print(f"Using prefix: {PREFIX}")
 ENV = PREFIX
 
@@ -29,20 +32,20 @@ if not GITHUB_BOT_TOKEN:
 GITHUB_APP_ID = os.environ.get("GITHUB_APP_ID")
 # deprecated: old logic transfer so upstream can use this
 if not GITHUB_APP_ID:
-    if PREFIX == "prod":
+    if ENVIRONMENT == "prod":
         GITHUB_APP_ID = "307814"
-    elif PREFIX == "dev":
+    elif ENVIRONMENT == "dev":
         GITHUB_APP_ID = "324098"
-    elif PREFIX == "dev2":
+    elif ENVIRONMENT == "dev2":
         GITHUB_APP_ID = "327588"
 GITHUB_BOT_USERNAME = os.environ.get("GITHUB_BOT_USERNAME")
 # deprecated: old logic transfer so upstream can use this
 if not GITHUB_BOT_USERNAME:
-    if PREFIX == "prod":
+    if ENVIRONMENT == "prod":
         GITHUB_BOT_USERNAME = "sweep-ai[bot]"
-    elif PREFIX == "dev":
+    elif ENVIRONMENT == "dev":
         GITHUB_BOT_USERNAME = "sweep-nightly[bot]"
-    elif PREFIX == "dev2":
+    elif ENVIRONMENT == "dev2":
         GITHUB_BOT_USERNAME = "sweep-canary[bot]"
 
 GITHUB_LABEL_NAME = os.environ.get("GITHUB_LABEL_NAME", "sweep")
