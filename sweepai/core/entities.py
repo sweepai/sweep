c33c9192b8063c9e98e1de54f487f6af8910f452
Lucas Jagg <lukejagg@gmail.com>
2023-08-21 13:28:49 -0500

    Moved exception to entities

--- sweepai/core/entities.py
+++ sweepai/core/entities.py
@@ -395,3 +395,8 @@ class SweepContext(BaseModel):
 
     def __str__(self):
         return self.issue_url
+
+
+class MaxTokensExceeded(Exception):
+    def __init__(self, filename):
+        self.filename = filename
