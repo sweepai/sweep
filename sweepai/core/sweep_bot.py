8023efdc3f5083a581cd80ddb399f6d591a2e0e3
Lucas Jagg <lukejagg@gmail.com>
2023-08-21 22:29:45 -0500

    New file prompt for linting on modify files

--- sweepai/core/sweep_bot.py
+++ sweepai/core/sweep_bot.py
@@ -32,6 +32,7 @@ from sweepai.core.prompts import (
     code_repair_modify_prompt,
     snippet_replacement,
     chunking_prompt,
+    linting_new_file_prompt,
     linting_modify_prompt,
 )
 from sweepai.config.client import SweepConfig, get_blocked_dirs
