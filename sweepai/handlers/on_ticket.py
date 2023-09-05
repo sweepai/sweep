a34e5e340425435c2dbebb944af94d39b0852688
Kevin Lu <kevinlu1248@gmail.com>
2023-09-05 14:12:16 -0700

    change to suffix

--- sweepai/handlers/on_ticket.py
+++ sweepai/handlers/on_ticket.py
@@ -65,7 +65,7 @@ bot_suffix_starring = (
 )
 bot_suffix = (
     f"\n{sep}\n{UPDATES_MESSAGE}\n{sep} 💡 To recreate the pull request edit the issue"
-    " title or description."
+    " title or description. To tweak the pull request, leave a comment on the pull request."
 )
 discord_suffix = f"\n<sup>[Join Our Discord](https://discord.com/invite/sweep)"
 
