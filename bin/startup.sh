4f2358972a4385fc2357246a9579c58405af84bf
wwzeng1 <william@sweep.dev>
2023-09-06 00:35:06 +0000

    decrease workers

--- bin/startup.sh
+++ bin/startup.sh
@@ -2,4 +2,4 @@
 
 redis-server --bind 0.0.0.0 --port 6379 &
 uvicorn sweepai.api:app --host 0.0.0.0 --port 8080 &
-celery -A sweepai.celery_init worker --loglevel=debug --pool=eventlet -c 100
+celery -A sweepai.celery_init worker --loglevel=debug --pool=eventlet -c 10
