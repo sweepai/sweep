8b72e270308b1e7ddef22591e5081a9d1e6054a7
Lucas Jaggernauth <lukejagg@gmail.com>
2023-09-06 00:21:53 -0500

    Install docker in droplet

--- bin/droplet_install.sh
+++ bin/droplet_install.sh
@@ -17,6 +17,7 @@ libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git
 sudo apt update
 sudo apt install -y gcc g++
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && sudo apt update && sudo apt install docker-ce -y && sudo systemctl enable docker && sudo systemctl start docker && sudo usermod -aG docker ${USER}
 
 # Install python 3.11.5
 pyenv install 3.11.5
