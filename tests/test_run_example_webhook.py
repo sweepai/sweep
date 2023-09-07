7345ffba045e0d7bddf273dd3b1c9ae8736c1438
wwzeng1 <william@sweep.dev>
2023-09-07 06:12:52 +0000

    update lexical search

---
+++ tests/test_run_example_webhook.py
@@ -0,0 +1,12 @@
+import json
+import requests
+import time
+
+port = "http://0.0.0.0:8080"
+
+response = requests.post(
+    port,
+    json=json.load(open("tests/issue_webhook.json", "r")),
+    headers={"X-GitHub-Event": "issues"},
+)
+print(response)
