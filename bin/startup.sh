d78b3870749447cdb3b103d02d0903cfab07c1e3
Lucas Jaggernauth <lukejagg@gmail.com>
2023-09-07 23:07:37 -0500

    Increased number of workers

--- bin/startup.sh
+++ bin/startup.sh
@@ -3,4 +3,4 @@
 echo PORT: ${PORT:-8080}
 redis-server /app/redis.conf --bind 0.0.0.0 --port 6379 &
 uvicorn sweepai.api:app --host 0.0.0.0 --port ${PORT:-8080} &
-celery -A sweepai.celery_init worker --loglevel=info --pool=eventlet -c 10
+celery -A sweepai.celery_init worker --loglevel=info --pool=eventlet -c 100
