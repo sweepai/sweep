6a1c41b05ba2b9e357874296e4399cdba4c61e9f
Kevin Lu <kevinlu1248@gmail.com>
2023-09-10 15:56:05 -0700

    changed deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -25,12 +25,12 @@ echo "Found open port: $PORT"
 # Start new docker container
 cd ~/sweep
 
-docker compose build
-docker run --name sandbox-web --env-file .env -v /var/run/docker.sock:/var/run/docker.sock -p 8081:8080 -d sweepai/sandbox-web
+docker compose webhook sandbox-web --build
+# docker run --name sandbox-web --env-file .env -v /var/run/docker.sock:/var/run/docker.sock -p 8081:8080 -d sweepai/sandbox-web
 
 # docker compose doesn't seem to build the sweepai/sweep image properly
-docker build -t sweepai/sweep:latest .
-docker run --env-file .env -p $PORT:8080 -d sweepai/sweep:latest
+# docker build -t sweepai/sweep:latest .
+# docker run --env-file .env -p $PORT:8080 -d sweepai/sweep:latest
 
 # Check if the "ngrok" screen session exists
 screen -list | grep -q "\bngrok\b"
