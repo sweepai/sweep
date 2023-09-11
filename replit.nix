f78014dc61f596afbd45316944a2fb88e1386b3a
Lucas Jaggernauth <lukejagg@gmail.com>
2023-09-11 18:58:30 -0500

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,6 +1,25 @@
 { pkgs }: {
   deps = [
-    pkgs.python310
+    pkgs.nodejs-19_x
+    pkgs.python310Full
+    pkgs.replitPackages.prybar-python310
+    pkgs.replitPackages.stderred
     pkgs.redis
   ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      # Needed for pandas / numpy
+      pkgs.stdenv.cc.cc.lib
+      pkgs.zlib
+      # Needed for pygame
+      pkgs.glib
+      # Needed for matplotlib
+      pkgs.xorg.libX11
+    ];
+    PYTHONHOME = "${pkgs.python310Full}";
+    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
+    LANG = "en_US.UTF-8";
+    STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
+    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
+  };
 }
