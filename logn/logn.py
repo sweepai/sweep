969f69aa24d64baec5b17f279022c4451b3c5a64
Lucas Jaggernauth <lukejagg@gmail.com>
2023-09-12 23:05:19 -0500

    Added task attribute

--- logn/logn.py
+++ logn/logn.py
@@ -17,7 +17,7 @@ def get_task_key():
 _task_dictionary = {}
 
 
-def _find_available_path(path, extension=".logn"):
+def _find_available_path(path, extension=".txt"):
     index = 0
     available_path = f"{path}{extension}"
     while os.path.exists(available_path):
@@ -28,17 +28,23 @@ def _find_available_path(path, extension=".logn"):
 
 
 class _Task:
-    def __init__(self, task_key, **metadata):
-        self.task_key = task_key
+    def __init__(self, logn_task_key, logn_parent_task=None, **metadata):
+        if logn_task_key is None:
+            logn_task_key = get_task_key()
+
+        self.task_key = logn_task_key
         self.metadata = {**metadata}
+        self.parent_task = logn_parent_task
+        if "name" not in self.metadata:
+            self.metadata["name"] = str(self.task_key.name.split(" ")[0])
         self.log_path, self.meta_path = self.create_files()
 
     @staticmethod
     def default():
-        return _Task(task_key=threading.current_thread())
+        return _Task(logn_task_key=threading.current_thread())
 
     def create_files(self):
-        name = str(self.metadata.get("name", str(self.task_key.name.split(" ")[0])))
+        name = self.metadata["name"]
 
         # Write logging file
         log_path = _find_available_path(os.path.join(LOG_PATH, name))
@@ -59,6 +65,10 @@ class _Task:
                         "logs": log_path,
                         "datetime": str(datetime.datetime.now()),
                         "metadata": self.metadata,
+                        # Todo: Write parent task in here
+                        "parent_task": self.parent_task.meta_path
+                        if self.parent_task is not None
+                        else None,
                     }
                 )
             )
@@ -73,6 +83,39 @@ class _Task:
             log = " ".join([str(arg) for arg in args])
             f.write(f"{log}{END_OF_LINE}")
 
+    @staticmethod
+    def get_task(task_key=None):
+        if task_key is None:
+            task_key = get_task_key()
+
+        if task_key in _task_dictionary:
+            task = _task_dictionary[task_key]
+        else:
+            task = _Task.default()
+            _task_dictionary[task_key] = task
+
+        return task
+
+    @staticmethod
+    def update_task(task_key=None, task=None):
+        if task_key is None:
+            task_key = get_task_key()
+
+        _task_dictionary[task_key] = task
+
+    @staticmethod
+    def create_child_task(name: str, **metadata):
+        parent_task = _Task.get_task()
+        child_task = _Task(
+            logn_task_key=parent_task.task_key,
+            logn_parent_task=parent_task,
+            **metadata,
+            **metadata,
+            name=parent_task.metadata["name"] + "_" + name,
+        )
+        _task_dictionary[parent_task.task_key] = child_task
+        return parent_task, child_task
+
 
 class _Logger:
     def __init__(self, printfn):
@@ -86,25 +129,8 @@ class _Logger:
 
         # write to file
         # Todo: Make task_key customizable
-        task_key = get_task_key()
-
-        if task_key in _task_dictionary:
-            task = _task_dictionary[task_key]
-        else:
-            task = _Task.default()
-            _task_dictionary[task_key] = task
-
+        task = _Task.get_task()
         task.write_log(*args, **kwargs)
-
-    @staticmethod
-    def set_task(task_key=None, **metadata):
-        global _task_dictionary
-        if task_key is None:
-            task_key = get_task_key()
-
-        new_task = _Task(task_key=task_key, **metadata)
-        _task_dictionary[task_key] = new_task
-        new_task.write_metadata()
 
 
 class _LogN(_Logger):
@@ -116,4 +142,32 @@ class _LogN(_Logger):
         return _Logger(printfn=printfn)
 
 
+class _LogTask:
+    def __init__(self, name):
+        self.name = name
+
+    def __call__(self, func):
+        def wrapper(*args, **kwargs):
+            print(self.name, f"Logging before calling {func.__name__}")
+
+            parent_task, child_task = _Task.create_child_task(name=self.name)
+
+            try:
+                result = func(*args, **kwargs)
+            except Exception as e:
+                _Task.update_task(task_key=parent_task.task_key, task=parent_task)
+                raise e
+
+            _Task.update_task(task_key=parent_task.task_key, task=parent_task)
+
+            print(self.name, f"Logging after calling {func.__name__}")
+            return result
+
+        return wrapper
+
+
+# Export logger
 logn_logger = _LogN()
+
+# Export method attribute
+LogTask = _LogTask
