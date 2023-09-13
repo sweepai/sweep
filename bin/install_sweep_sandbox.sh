2bcf19b477d97eb612415df86571768f2f611bcf
Kevin Lu <kevinlu1248@gmail.com>
2023-09-13 16:37:07 -0700

    install_sweep_sandbox.sh

--- bin/install_sweep_sandbox.sh
+++ bin/install_sweep_sandbox.sh
@@ -4,10 +4,10 @@
 # CYAN='\033[1;36m'
 CYAN=''
 # WHITE='\033[1;37m'
-WHITE='\033[1;37m '
+WHITE='\033[1;37m'
 # WHITE='\033[1;30m\033[47m'
 RED='\033[0;31m'
-NC=' \033[0m' # No Color
+NC='\033[0m' # No Color
 
 echo -e "\n${CYAN}${WHITE}### Starting Setup Script ###${NC}\n"
 
