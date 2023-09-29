b54be2243630a6f1c9e3da9403ed166ceddaf59a
Kevin Lu <kevinlu1248@gmail.com>
2023-09-28 23:40:38 -0700

    Added new blog

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,7 +1,8 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  defaultShowCopyCode: true
+  defaultShowCopyCode: true,
+  latex: true
 })
 
 module.exports = withNextra()
