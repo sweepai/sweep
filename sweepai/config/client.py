c837519e3cb10b6fde62db00de8b5db38da247ca
wwzeng1 <william@sweep.dev>
2023-10-03 21:59:11 +0000

    fix config

--- sweepai/config/client.py
+++ sweepai/config/client.py
@@ -11,8 +11,6 @@ from pydantic import BaseModel
 from logn import logger
 from sweepai.core.entities import EmptyRepository
 
-branch_overrides = {}  # type: Dict[(Thread, str), str]
-
 
 class SweepConfig(BaseModel):
     include_dirs: list[str] = []
@@ -115,34 +113,30 @@ class SweepConfig(BaseModel):
 
         default_branch = repo.default_branch
         try:
+            sweep_yaml_dict = {}
             try:
                 contents = repo.get_contents("sweep.yaml")
                 sweep_yaml_dict = yaml.safe_load(contents.decoded_content.decode("utf-8"))
             except SystemExit:
                 raise SystemExit
-                
-            if "branch" in sweep_yaml_dict:
-                branch_name = sweep_yaml_dict["branch"]
-                try:
-                    repo.get_branch(branch_name)
-                    return branch_name
-                except SystemExit:
-                    raise SystemExit
-                except Exception as e:
-                    logger.warning(f"Error when getting branch: {e}, creating branch")
-                    repo.create_git_ref(
-                        f"refs/heads/{branch_name}",
-                        repo.get_branch(default_branch).commit.sha,
-                    )
-                    return branch_name
-            else:
+            if "branch" not in sweep_yaml_dict:
                 return default_branch
+            branch_name = sweep_yaml_dict["branch"]
+            try:
+                repo.get_branch(branch_name)
+                return branch_name
+            except SystemExit:
+                raise SystemExit
+            except Exception as e:
+                logger.warning(f"Error when getting branch: {e}, creating branch")
+                repo.create_git_ref(
+                    f"refs/heads/{branch_name}",
+                    repo.get_branch(default_branch).commit.sha,
+                )
+                return branch_name
         except SystemExit:
             raise SystemExit
         except Exception as e:
-            logger.info(
-                f"Error when getting branch: {e}, falling back to default branch"
-            )
             return default_branch
 
     @staticmethod
@@ -183,20 +177,6 @@ def get_gha_enabled(repo: Repository) -> bool:
     except SystemExit:
         raise SystemExit
     except Exception:
-        try:
-            contents = repo.get_contents(".github/sweep.yaml")
-        except SystemExit:
-            raise SystemExit
-        except Exception:
-            try:
-                contents = repo.get_contents(".github/sweep.yaml")
-            except SystemExit:
-                raise SystemExit
-            except Exception as e:
-                logger.warning(
-                    f"Error when getting gha enabled: {e}, falling back to True"
-                )
-                return True
         gha_enabled = yaml.safe_load(contents.decoded_content.decode("utf-8")).get(
             "gha_enabled", True
         )
