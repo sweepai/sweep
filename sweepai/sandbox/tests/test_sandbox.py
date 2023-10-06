de4174b44b62c9849d4ce7f4340647db3b460c21
Kevin Lu <kevinlu1248@gmail.com>
2023-10-05 22:09:36 -0700

    Improvements to sandbox

--- sweepai/sandbox/tests/test_sandbox.py
+++ sweepai/sandbox/tests/test_sandbox.py
@@ -1,14 +1,14 @@
 from fastapi.testclient import TestClient
 from src.sandbox_local import app
-from test_data import bad_file_contents, file_path
 
 client = TestClient(app)
 
 if __name__ == "__main__":
     data = {
-        "repo_url": "https://github.com/sweepai/landing-page",
-        "file_path": file_path,
-        "content": bad_file_contents,
+        # "repo_url": "https://github.com/sweepai/landing-page",
+        "repo_url": "https://github.com/kevinlu1248/flowiseembed2"
+        # "file_path": file_path,
+        # "content": bad_file_contents,
     }
     response = client.post("/", json=data)
     print(response.text)
