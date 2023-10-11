358908c7b6b55737ab428aa9ee97d65cb7ae426f
Kevin Lu <kevinlu1248@gmail.com>
2023-10-10 23:02:12 -0700

    added replicate thing

--- sweepai/core/vector_db.py
+++ sweepai/core/vector_db.py
@@ -22,7 +22,7 @@ from sweepai.config.server import (
     HUGGINGFACE_URL,
     REDIS_URL,
     REPLICATE_API_KEY,
-    REPLICATE_URL,
+    REPLICATE_DEPLOYMENT_URL,
     SENTENCE_TRANSFORMERS_MODEL,
     VECTOR_EMBEDDING_SOURCE,
 )
@@ -87,31 +87,34 @@ def embed_huggingface(texts):
             )
 
 
+def embed_replicate(texts):
+    client = replicate.Client(api_token=REPLICATE_API_KEY)
+    deployment = client.deployments.get(REPLICATE_DEPLOYMENT_URL)
+    for i in range(3):
+        try:
+            prediction = deployment.predictions.create(
+                input={"text_batch": json.dumps(texts)}, timeout=60
+            )
+            prediction.wait()
+            outputs = prediction.output
+            break
+        except Exception as e:
+            logger.exception(f"Replicate timeout: {e}")
+    else:
+        raise Exception(f"Replicate timeout {e}")
+    return [output["embedding"] for output in outputs]
+
+
 # def embed_replicate(texts):
 #     client = replicate.Client(api_token=REPLICATE_API_KEY)
-#     deployment = client.deployments.get(REPLICATE_URL)
 #     for i in range(3):
 #         try:
-#             prediction = deployment.predictions.create(
+#             outputs = client.run(
 #                 REPLICATE_URL, input={"text_batch": json.dumps(texts)}, timeout=60
 #             )
-#             prediction.wait()
-#             outputs = prediction.output
 #         except Exception as e:
 #             logger.exception(f"Replicate timeout: {e}")
 #     return [output["embedding"] for output in outputs]
-
-
-def embed_replicate(texts):
-    client = replicate.Client(api_token=REPLICATE_API_KEY)
-    for i in range(3):
-        try:
-            outputs = client.run(
-                REPLICATE_URL, input={"text_batch": json.dumps(texts)}, timeout=60
-            )
-        except Exception as e:
-            logger.exception(f"Replicate timeout: {e}")
-    return [output["embedding"] for output in outputs]
 
 
 @lru_cache(maxsize=64)
