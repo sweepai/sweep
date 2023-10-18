d5490e0054540f0f064e584e5d56410c7945c4c1
wwzeng1 <william@sweep.dev>
2023-10-18 22:40:01 +0000

    update

--- sweepai/core/prompts.py
+++ sweepai/core/prompts.py
@@ -1079,7 +1079,7 @@ File path: {file_path}
 {code}
 ```
 </old_code>
-
+{changes_made}
 # Request
 {request}
 
@@ -1140,18 +1140,59 @@ Respond in the following format:
 
 <snippets_and_plan_analysis file="file_path">
 Describe what should be changed to the snippets from the old_file to complete the request.
-Then, for each snippet in a list, determine whether changes should be made. If so, describe the changes needed. Otherwise, do not write an updated_snippet block for this snippet.
+Then, for each snippet in a list, determine whether changes should be made. If so, describe the changes needed, with references to the lines that should be changed and what to change it to. Otherwise, do not write an updated_snippet block for this snippet.
 Maximize information density.
 </snippets_and_plan_analysis>
 
-<updated_snippets>
-<updated_snippet index="i">
+<diffs>
 ```
-new code to replace the entirety of the old code
+<<<<<<< ORIGINAL (index=i)
+old line(s) from the snippet
+=======
+new line(s) to replace
+>>>>>>> UPDATED
+
+<<<<<<< ORIGINAL (index=j)
+old line(s) from the snippet
+=======
+new line(s) to replace
+>>>>>>> UPDATED
 ```
-</updated_snippet>
 ...
-</updated_snippets>"""
+</diffs>"""
+
+update_snippets_system_prompt_python = """\
+You are a brilliant and meticulous engineer assigned to write code to complete the user's request. You specialize in Python programming. When you write code, the code works on the first try, is syntactically perfect, and is complete. Ensure correct indentation for each indentation level, as per PEP 8. Place all 'from ... import ...' and 'import ...' statements at the beginning of the file.
+
+You have the utmost care for the code that you write, so you do not make mistakes and you fully implement every function and class. Take into account the current repository's language, code style, and dependencies.
+
+You will be given the old_file and potentially relevant snippets to edit. You do not necessarily have to edit all the snippets.
+
+Respond in the following format:
+
+<snippets_and_plan_analysis>
+Describe what should be changed to the snippets from the old_file to complete the request.
+Then, for each snippet in a list, determine whether changes should be made. If so, describe the changes needed, with references to the lines that should be changed and what to change it to. Otherwise, do not write an updated_snippet block for this snippet.
+Maximize information density.
+</snippets_and_plan_analysis>
+
+<diffs>
+>>>>>>> main
+```
+<<<<<<< ORIGINAL (index=i)
+old line(s) from the snippet
+=======
+new line(s) to replace
+>>>>>>> UPDATED
+
+<<<<<<< ORIGINAL (index=j)
+old line(s) from the snippet
+=======
+new line(s) to replace
+>>>>>>> UPDATED
+```
+...
+</diffs>"""
 
 update_snippets_prompt = """# Code
 File path: {file_path}
@@ -1160,7 +1201,7 @@ File path: {file_path}
 {code}
 ```
 </old_code>
-
+{changes_made}
 # Request
 {request}
 
@@ -1171,25 +1212,35 @@ File path: {file_path}
 # Instructions
 Rewrite each of the {n} snippets above according to the request.
 * Do not delete whitespace or comments.
-* To delete code insert an empty string.
+* Write minimal diff hunks to make changes to the snippets. Only write diffs for the lines that should be changed.
+* Write multiple smalle changes instead of a single large change.
 * To add code before and after the snippet, be sure to copy the original snippet.
 
 Respond in the following format:
 
 <snippets_and_plan_analysis file="file_path">
 Describe what should be changed to the snippets from the old_file to complete the request.
-Then, for each snippet in a list, determine whether changes should be made. If so, describe the changes needed. Otherwise, do not write an updated_snippet block for this snippet.
+Then, for each snippet in a list, determine whether changes should be made. If so, describe the changes needed, with references to the lines that should be changed and what to change it to. Otherwise, do not write an updated_snippet block for this snippet.
 Maximize information density.
 </snippets_and_plan_analysis>
 
-<updated_snippets>
-<updated_snippet index="i">
+<diffs>
 ```
-new code to replace the entirety of the old code
+<<<<<<< ORIGINAL (index=i)
+old line(s) from the snippet
+=======
+new line(s) to replace
+>>>>>>> UPDATED
+
+<<<<<<< ORIGINAL (index=j)
+old line(s) from the snippet
+=======
+new line(s) to replace
+>>>>>>> UPDATED
 ```
-</updated_snippet>
 ...
-</updated_snippets>"""
+
+</diffs>"""
 
 update_snippets_prompt_test = """# Code
 File path: {file_path}
@@ -1219,11 +1270,20 @@ For each snippet in a list, determine whether changes should be made. If so, des
 Maximize information density.
 </snippets_and_plan_analysis>
 
-<updated_snippets>
-<updated_snippet index="i">
+<diffs>
 ```
-new code to replace the entirety of the old code
+<<<<<<< ORIGINAL (index=i)
+old line(s) from the snippet
+=======
+new line(s) to replace
+>>>>>>> UPDATED
+
+<<<<<<< ORIGINAL (index=j)
+old line(s) from the snippet
+=======
+new line(s) to replace
+>>>>>>> UPDATED
 ```
-</updated_snippet>
 ...
-</updated_snippets>"""
+
+</diffs>"""
