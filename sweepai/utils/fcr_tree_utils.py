bae8045f4b414fb3434e13006f6bf890a6fafd3f
wwzeng1 <william@sweep.dev>
2023-10-23 23:29:58 +0000

    new colors

--- sweepai/utils/fcr_tree_utils.py
+++ sweepai/utils/fcr_tree_utils.py
@@ -5,10 +5,15 @@ from sweepai.core.entities import FileChangeRequest
 
 def create_digraph(file_change_requests: list[FileChangeRequest]):
     dot = Digraph(comment="FileChangeRequest Tree")
-    fontname = "sans-serif"
+    fontname = "helvetica"
     dot.attr(fontname=fontname)
     dot.attr(pad="0.5")
     dot.attr(label="Sweep's Plan & Progress\n\n", labelloc="t", labeljust="c")
+    dot.attr(bgcolor='dimgray') # set background color
+    
+    dot.edge_attr.update(color='white')
+    node_fontcolor = "black"
+    ends_fontcolor = "black"
 
     ranks = {}
 
@@ -33,11 +38,13 @@ def create_digraph(file_change_requests: list[FileChangeRequest]):
                     labeljust="l",
                     rank="same",
                     fontname=fontname,
+                    fontcolor=node_fontcolor,
+                    color=node_fontcolor,
                 )
                 c.node("start", "", shape="none", width="0")
                 c.node("end", "", shape="none", width="0")
             else:
-                c.attr(label=f"Layer {layer}", rank="same", fontname=fontname)
+                c.attr(label=f"Layer {layer}", rank="same", fontname=fontname, fontcolor=node_fontcolor, color=node_fontcolor)
             for fcr in file_change_requests:
                 if ranks[fcr.id_] == layer:
                     if fcr.change_type == "check":
@@ -48,6 +55,8 @@ def create_digraph(file_change_requests: list[FileChangeRequest]):
                             fillcolor=fcr.color,
                             style="filled",
                             fontname=fontname,
+                            fontcolor=node_fontcolor,
+                            color=node_fontcolor,
                         )
                     else:
                         c.node(
@@ -56,6 +65,8 @@ def create_digraph(file_change_requests: list[FileChangeRequest]):
                             fillcolor=fcr.color,
                             style="filled",
                             fontname=fontname,
+                            fontcolor=node_fontcolor,
+                            color=node_fontcolor,
                         )
 
     last_item_per_layer = {layer: None for layer in range(max(ranks.values()) + 1)}
@@ -63,24 +74,24 @@ def create_digraph(file_change_requests: list[FileChangeRequest]):
     for fcr in file_change_requests:
         if fcr.parent:
             if fcr.change_type == "check":
-                dot.edge(fcr.parent.id_, fcr.id_, style="dashed", fontname=fontname)
+                dot.edge(fcr.parent.id_, fcr.id_, style="dashed", fontname=fontname, fontcolor=node_fontcolor, color=node_fontcolor)
             elif fcr.parent.change_type == "check":
                 dot.edge(
                     fcr.parent.id_,
                     fcr.id_,
                     label="Additional changes required",
                     fontname=fontname,
-                )
+                    fontcolor=node_fontcolor, 
+                    color=node_fontcolor)
             else:
-                dot.edge(fcr.parent.id_, fcr.id_, fontname=fontname)
+                dot.edge(fcr.parent.id_, fcr.id_, fontname=fontname, fontcolor=node_fontcolor, color=node_fontcolor)
         elif last_item_per_layer[ranks[fcr.id_]] is not None:
             dot.edge(
-                last_item_per_layer[ranks[fcr.id_]].id_, fcr.id_, fontname=fontname
-            )
+                last_item_per_layer[ranks[fcr.id_]].id_, fcr.id_, fontname=fontname, fontcolor=node_fontcolor, color=node_fontcolor)
         last_item_per_layer[ranks[fcr.id_]] = fcr
 
-    dot.edge("start", file_change_requests[0].id_, label="Start", fontname=fontname)
-    dot.edge(last_fcr.id_, "end", label="Finish", fontname=fontname)
+    dot.edge("start", file_change_requests[0].id_, label="Start", fontname=fontname, fontcolor=ends_fontcolor, color=ends_fontcolor)
+    dot.edge(last_fcr.id_, "end", label="Finish", fontname=fontname, fontcolor=ends_fontcolor, color=ends_fontcolor)
 
     return dot
 
