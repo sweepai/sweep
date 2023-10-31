1bee288a6283c81561e39030d8db2223a4c1d456
Kevin Lu <kevinlu1248@gmail.com>
2023-10-31 12:23:39 -0700

    Done move bot

--- sweepai/agents/move_bot.py
+++ sweepai/agents/move_bot.py
@@ -5,12 +5,19 @@ from rope.refactor.move import MoveGlobal
 
 from sweepai.core.chat import ChatGPT
 from sweepai.core.entities import Message
-from sweepai.core.update_prompts import (
-    extract_snippets_system_prompt,
-    extract_snippets_user_prompt,
-)
 from sweepai.utils.github_utils import ClonedRepo
-from sweepai.utils.search_and_replace import find_best_match
+
+system_prompt = """You are a brilliant and meticulous engineer assigned to decide where to move code to help the user refactor their codebase. You specialize in Python programming. Take into account the current repository's language, code style, and dependencies.
+
+You will be given the old_file and relevant snippets to edit. Respond in the following format:
+
+<moves>
+<move
+    from_file="src/utils/main.py"
+    entity="foo"
+    destination_module="src.utils.foo_utils"
+/>
+</moves>"""
 
 user_prompt = """# Code
 File path: {file_path}
@@ -23,20 +30,18 @@ File path: {file_path}
 # Request
 {request}
 
-<snippets_to_update>
-{snippets}
-</snippets_to_update>
-
 # Instructions
 Indicate which snippets should be moved to the destination module.
 
 Respond in the following format:
 
+<moves>
 <move
     from_file="src/utils/main.py"
-    entity_name="foo"
+    entity="foo"
     destination_module="src.utils.foo_utils"
 />
+</moves>
 """
 
 
@@ -61,9 +66,8 @@ class MoveBot(ChatGPT):
     def refactor_snippets(
         self,
         additional_messages: list[Message] = [],
-        snippets_str="",
         file_path: str = "",
-        update_snippets_code: str = "",
+        contents: str = "",
         request="",
         changes_made="",
         cloned_repo: ClonedRepo = None,
@@ -77,63 +81,30 @@ class MoveBot(ChatGPT):
         self.messages = [
             Message(
                 role="system",
-                content=extract_snippets_system_prompt,
+                content=system_prompt,
                 key="system",
             )
         ]
         self.messages.extend(additional_messages)
-        extract_response = self.chat(
-            extract_snippets_user_prompt.format(
-                code=update_snippets_code,
+        move_response = self.chat(
+            user_prompt.format(
+                code=contents,
                 file_path=file_path,
-                snippets=snippets_str,
                 request=request,
                 changes_made=changes_made,
             )
         )
-        new_function_pattern = (
-            r"<new_function_names>\s+(?P<new_function_names>.*?)</new_function_names>"
-        )
-        new_function_matches = list(
-            re.finditer(new_function_pattern, extract_response, re.DOTALL)
-        )
-        new_function_names = []
-        for match_ in new_function_matches:
-            match = match_.groupdict()
-            new_function_names = match["new_function_names"]
-            new_function_names = new_function_names.split("\n")
-        new_function_names = [
-            new_function_name.strip().strip('"').strip("'").strip("`")
-            for new_function_name in new_function_names
-            if new_function_name.strip()
-        ]
-        extracted_pattern = r"<<<<<<<\s+EXTRACT\s+(?P<updated_code>.*?)>>>>>>>"
-        extract_matches = list(
-            re.finditer(extracted_pattern, extract_response, re.DOTALL)
-        )
+        move_pattern = r'<move\s+from_file="(?P<from_file>.*?)"\s+entity="(?P<entity>.*?)"\s+destination_module="(?P<destination_module>.*?)"\s+/>'
+        move_matches = list(re.finditer(move_pattern, move_response, re.DOTALL))
         change_sets = []
         new_code = None
-        for idx, match_ in enumerate(extract_matches):
+        for idx, match_ in enumerate(move_matches):
             match = match_.groupdict()
-            updated_code = match["updated_code"]
-            updated_code = updated_code.strip("\n")
-            best_match = find_best_match(updated_code, snippets_str)
-            if best_match.score < 70:
-                updated_code = "\n".join(updated_code.split("\n")[1:])
-                best_match = find_best_match(updated_code, snippets_str)
-                if best_match.score < 80:
-                    updated_code = "\n".join(updated_code.split("\n")[:-1])
-                    best_match = find_best_match(updated_code, snippets_str)
-                    if best_match.score < 80:
-                        continue
-            extracted_original_code = "\n".join(
-                snippets_str.split("\n")[best_match.start : best_match.end]
-            )
+            from_file = match["from_file"]
+            entity = match["entity"]
+            destination_module = match["destination_module"]
             new_code, change_set = move_function(
-                extracted_original_code,
-                file_path,
-                new_function_names[idx],
-                project_name=cloned_repo.cache_dir,
+                from_file, entity, destination_module, cloned_repo.cache_dir
             )
             change_sets.append(change_set)
         for change_set in change_sets:
@@ -154,58 +125,12 @@ _No response_""",
             key="user",
         )
     ]
-    snippets_str = """\
-<snippet index="0">
-def compute_deeplake_vs(collection_name, documents, ids, metadatas, sha):
-    if len(documents) > 0:
-        logger.info(f"Computing embeddings with {VECTOR_EMBEDDING_SOURCE}...")
-        # Check cache here for all documents
-        embeddings = [None] * len(documents)
-        if redis_client:
-            cache_keys = [
-                hash_sha256(doc)
-                + SENTENCE_TRANSFORMERS_MODEL
-                + VECTOR_EMBEDDING_SOURCE
-                + CACHE_VERSION
-                for doc in documents
-            ]
-            cache_values = redis_client.mget(cache_keys)
-            for idx, value in enumerate(cache_values):
-                if value is not None:
-                    arr = json.loads(value)
-                    if isinstance(arr, list):
-                        embeddings[idx] = np.array(arr, dtype=np.float32)
-        logger.info(
-            f"Found {len([x for x in embeddings if x is not None])} embeddings in cache"
-        )
-        indices_to_compute = [idx for idx, x in enumerate(embeddings) if x is None]
-        documents_to_compute = [documents[idx] for idx in indices_to_compute]
-        logger.info(f"Computing {len(documents_to_compute)} embeddings...")
-        computed_embeddings = embedding_function(documents_to_compute)
-        logger.info(f"Computed {len(computed_embeddings)} embeddings")
-
-        for idx, embedding in zip(indices_to_compute, computed_embeddings):
-            embeddings[idx] = embedding
-
-        try:
-            embeddings = np.array(embeddings, dtype=np.float32)
-        except SystemExit:
-            raise SystemExit
-        except:
-            logger.exception(
-                "Failed to convert embeddings to numpy array, recomputing all of them"
-            )
-            embeddings = embedding_function(documents)
-            embeddings = np.array(embeddings, dtype=np.float32)
-</snippet>
-"""
     file_path = "sweepai/core/vector_db.py"
-    request = "Break this function into smaller sub-functions"
+    request = "* Move the `get_comment_header` and `custom_config` functions from `sweepai/handlers/on_ticket.py` to `sweepai/utils/ticket_utils.py`.\n\n* Ensure that the functions are placed in a logical location within `ticket_utils.py`, such as at the end of the file."
     changes_made = ""
-    bot = RefactorBot()
+    bot = MoveBot()
     bot.refactor_snippets(
         additional_messages=additional_messages,
-        snippets_str=snippets_str,
         file_path=file_path,
         request=request,
         changes_made=changes_made,
