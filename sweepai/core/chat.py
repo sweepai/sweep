793ea9b5e976ccfe6c03fa787bdf13d746c36d5c
Kevin Lu <kevinlu1248@gmail.com>
2023-11-08 00:04:12 -0800

    Error with 16k and max token count

--- sweepai/core/chat.py
+++ sweepai/core/chat.py
@@ -40,6 +40,7 @@ ChatModel = OpenAIModel
 model_to_max_tokens = {
     "gpt-3.5-turbo": 4096,
     "gpt-3.5-turbo-1106": 16385,
+    "gpt-3.5-turbo-16k": 16385,
     "gpt-4": 8192,
     "gpt-4-1106-preview": 128000,
     "gpt-4-0613": 8192,
