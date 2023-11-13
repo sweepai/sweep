13ebfa00e3b25d91161b097ae8b5a8779743cfe6
wwzeng1 <william@sweep.dev>
2023-11-13 21:07:45 +0000

    bugfix

--- sweepai/config/client.py
+++ sweepai/config/client.py
@@ -272,7 +272,7 @@ def get_rules(repo: Repository):
         )
         sweep_yaml = yaml.safe_load(sweep_yaml_content)
         rules = sweep_yaml.get("rules", [])
-        return [rule for rule in rules if rule.strip()]
+        return rules
     except SystemExit:
         raise SystemExit
     except Exception:
