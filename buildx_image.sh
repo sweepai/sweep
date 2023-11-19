a3a08e93ee0a763ab1e62069bcf7272993e67bf3
Kevin Lu <kevinlu1248@gmail.com>
2023-11-18 18:24:23 -0800

    Deleted useless files

--- buildx_image.sh
+++
@@ -1,3 +0,0 @@
-#docker buildx create --platform linux/amd64
-docker buildx build --platform linux/amd64 -t sweepai/sweep:latest .
-#docker run --platform linux/amd64 --env-file .env -p 8080:8080 sweepai/sweep:latest
--- run_image.sh
+++
@@ -1,1 +0,0 @@
-docker run --env-file .env -p 8080:8080 sweepai/sweep:latest
