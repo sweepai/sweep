d88187e4a5be7a1c61bb4bde588b12a8a9453e0a
Kevin Lu <kevinlu1248@gmail.com>
2023-11-27 02:39:21 -0800

    Indexing error

--- sweepai/agents/assistant_wrapper.py
+++ sweepai/agents/assistant_wrapper.py
@@ -35,7 +35,7 @@ def get_json_messages(
     messages_json = [system_message_json]
     for message_obj in list(
         client.beta.threads.runs.steps.list(run_id=run_id, thread_id=thread_id).data
-    )[:-1:-1]:
+    )[:0:-1]:
         if message_obj.type == "message_creation":
             message_id = message_obj.step_details.message_creation.message_id
             message_content = (
