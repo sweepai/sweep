533fb55f66feb4f269642bf4ad6d6dcd3ac448b0
William Zeng <44910023+wwzeng1@users.noreply.github.com>
2023-12-14 15:16:18 -0800

    Update filter_agent.py

--- sweepai/agents/filter_agent.py
+++ sweepai/agents/filter_agent.py
@@ -9,14 +9,14 @@ class FilterBot(ChatGPT):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.prompt = """\
-        <search_query>
-        {search_query}
-        </search_query>
-        Filter out unnecessary terms from the above search query and return a refined query.
-        <filtered_query>
-        refined_query
-        </filtered_query>
-        ..."""
+<search_query>
+{search_query}
+</search_query>
+Filter out unnecessary terms from the above search query and return a refined query.
+<filtered_query>
+refined_query
+</filtered_query>
+..."""
 
     def filter_query(self, search_query):
         filter_response = self.chat(
