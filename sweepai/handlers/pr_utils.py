55fabc7e5a371280363536c0a3effb7490f1502d
Kevin Lu <kevinlu1248@gmail.com>
2023-12-26 10:05:37 -0800

    Cleanup

--- sweepai/handlers/pr_utils.py
+++ sweepai/handlers/pr_utils.py
@@ -150,7 +150,9 @@ def make_pr(
         if DISCORD_FEEDBACK_WEBHOOK_URL is not None
         else ""
     )
-    rule_description = f'### I created this PR to address this rule: \n"{rule}"\n'
+    rule_description = (
+        f'### I created this PR to address this rule: \n"{rule}"\n' if rule else ""
+    )
     pr = repo.create_pull(
         title=pr_changes.title,
         body=pr_actions_message + rule_description + pr_changes.body,
