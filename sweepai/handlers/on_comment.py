6a5417cba4d92e8592b7355af14994d6d453a1a8
wwzeng1 <william@sweep.dev>
2023-12-28 23:25:16 +0000

    add fire and forget wrapper

--- sweepai/handlers/on_comment.py
+++ sweepai/handlers/on_comment.py
@@ -30,7 +30,7 @@ from sweepai.utils.event_logger import posthog
 from sweepai.utils.github_utils import ClonedRepo, get_github_client
 from sweepai.utils.progress import TicketProgress
 from sweepai.utils.prompt_constructor import HumanMessageCommentPrompt
-from sweepai.utils.ticket_utils import prep_snippets
+from sweepai.utils.ticket_utils import fire_and_forget_wrapper, prep_snippets
 
 num_of_snippets_to_query = 30
 total_number_of_snippet_tokens = 15_000
@@ -468,7 +468,7 @@ def on_comment(
             pass
 
     elapsed_time = time.time() - start_time
-    posthog.capture(
+    fire_and_forget_wrapper(posthog.capture)(
         username,
         "success",
         properties={**metadata, "duration": elapsed_time, "tracking_id": tracking_id},
