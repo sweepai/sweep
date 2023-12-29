529b8518fcaad5af4c6f972d7a35126d5d3c39b6
Kevin Lu <kevinlu1248@gmail.com>
2023-12-28 16:02:35 -0800

    API fixes

--- sweepai/api.py
+++ sweepai/api.py
@@ -839,28 +839,32 @@ async def webhook(raw_request: Request):
                                 request_dict["repository"]["full_name"],
                                 installation_id=request_dict["installation"]["id"],
                             )
+                    if ref.startswith("refs/heads"):
+                        branch_name = ref[len("refs/heads/") :]
+                        # Check if the branch has an associated PR
 
-                            branch_name = ref[len("refs/heads/") :]
-                            # Check if the branch has an associated PR
-                            pulls = repo.get_pulls(
-                                state="open", sort="created", base=branch_name
+                        org_name, repo_name = request_dict["repository"][
+                            "full_name"
+                        ].split("/")
+                        pulls = repo.get_pulls(
+                            state="open",
+                            sort="created",
+                            head=org_name + ":" + branch_name,
+                        )
+                        for pr in pulls:
+                            logger.info(
+                                f"PR associated with branch {branch_name}: #{pr.number} - {pr.title}"
                             )
-                            for pr in pulls:
-                                logger.info(
-                                    f"PR associated with branch {branch_name}: #{pr.number} - {pr.title}"
+                            if pr.mergeable == False:
+                                on_merge_conflict(
+                                    pr_number=pr.number,
+                                    username=pr.user.login,
+                                    repo_full_name=request_dict["repository"][
+                                        "full_name"
+                                    ],
+                                    installation_id=request_dict["installation"]["id"],
+                                    tracking_id=get_hash(),
                                 )
-                                if pr.mergeable == False:
-                                    on_merge_conflict(
-                                        pr_number=pr.number,
-                                        username=pr.user.login,
-                                        repo_full_name=request_dict["repository"][
-                                            "full_name"
-                                        ],
-                                        installation_id=request_dict["installation"][
-                                            "id"
-                                        ],
-                                        tracking_id=get_hash(),
-                                    )
             case "ping", None:
                 return {"message": "pong"}
     except ValidationError as e:
