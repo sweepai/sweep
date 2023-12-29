1b7b39e081a3bc7dbecbaeee25d73ea3585e481d
William Zeng <44910023+wwzeng1@users.noreply.github.com>
2023-12-28 18:09:46 -0800

    Revert "Sweep: Resolve merge conflicts for PR #2839: Remove unnecessary log" (#2842)
    Reverts sweepai/sweep#2840

--- sweepai/handlers/on_ticket.py
+++ sweepai/handlers/on_ticket.py
@@ -273,7 +273,6 @@ def on_ticket(
 
     try:
         if current_issue.state == "closed":
-            logger.warning(f"Issue {issue_number} is closed (tracking ID: `{tracking_id}`). Please join our Discord server for support (tracking_id={tracking_id})")
             posthog.capture(
                 username,
                 "issue_closed",
