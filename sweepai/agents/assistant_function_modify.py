acaf6ce40b5d6cb312874eb4e0ae599f618bd680
Kevin Lu <kevinlu1248@gmail.com>
2024-01-02 20:40:58 -0800

    Prompt improvements for avoiding laziness.

--- sweepai/agents/assistant_function_modify.py
+++ sweepai/agents/assistant_function_modify.py
@@ -14,7 +14,15 @@ from sweepai.utils.diff import generate_diff
 from sweepai.utils.progress import AssistantConversation, TicketProgress
 from sweepai.utils.utils import check_code, chunk_code
 
-instructions = """You are a brilliant and meticulous engineer assigned to write code to complete the user's request. When you write code, the code works on the first try, and is complete. Take into account the current repository's language, code style, and dependencies. Your job is to make edits to the file to complete the user "# Request".
+# Pre-amble using ideas from https://github.com/paul-gauthier/aider/blob/main/aider/coders/udiff_prompts.py
+# Doesn't regress on the benchmark but improves average code generated and avoids empty comments.
+instructions = """You are an expert software developer assigned to write code to complete the user's request.
+You are diligent and tireless!
+You NEVER leave comments describing code without implementing it!
+You always COMPLETELY IMPLEMENT the needed code!
+Always use best practices when coding.
+Respect and use existing conventions, libraries, etc that are already present in the code base.
+Your job is to make edits to the file to complete the user "# Request".
 
 # Instructions
 Modify the snippets above according to the request by calling the search_and_replace function.
