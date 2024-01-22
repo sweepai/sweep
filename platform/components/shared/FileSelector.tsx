0ed716fb059b2035d7e6ba6a538e1d3fe827424f
martin ye <martinye022@gmail.com>
2024-01-22 21:59:06 +0000

    combobox

--- platform/components/shared/FileSelector.tsx
+++ platform/components/shared/FileSelector.tsx
@@ -1,19 +1,77 @@
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
+import { cn } from "@/lib/utils";
 import React from "react";
+import { Button } from "../ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "../ui/command";
+import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons"
+
+const frameworks = [
+    {
+      value: "next.js",
+      label: "Next.js",
+    },
+    {
+      value: "sveltekit",
+      label: "SvelteKit",
+    },
+    {
+      value: "nuxt.js",
+      label: "Nuxt.js",
+    },
+    {
+      value: "remix",
+      label: "Remix",
+    },
+    {
+      value: "astro",
+      label: "Astro",
+    },
+  ]
 
 const FileSelector = () => {
+    const [open, setOpen] = React.useState(false)
+    const [value, setValue] = React.useState("")
     return (
-        <Select>
-            <SelectTrigger className="w-full">
-            <SelectValue placeholder="File path" />
-            </SelectTrigger>
-            <SelectContent>
-            <SelectItem value="light">Light</SelectItem>
-            <SelectItem value="dark">Dark</SelectItem>
-            <SelectItem value="system">System</SelectItem>
-            </SelectContent>
-        </Select>
-    );
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    className="w-[200px] justify-between">
+                    {value ? frameworks.find((framework) => framework.value === value)?.label : "Select framework..."}
+                    <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+                <Command>
+                    <CommandInput placeholder="Search framework..." className="h-9" />
+                    <CommandEmpty>No framework found.</CommandEmpty>
+                    <CommandGroup>
+                        {frameworks.map((framework) => (
+                            <CommandItem
+                                key={framework.value}
+                                value={framework.value}
+                                onSelect={(currentValue) => {
+                                    setValue(currentValue === value ? "" : currentValue)
+                                    setOpen(false)
+                                }}
+                            >
+                            {framework.label}
+                                <CheckIcon
+                                    className={cn(
+                                        "ml-auto h-4 w-4",
+                                        value === framework.value ? "opacity-100" : "opacity-0"
+                                    )}
+                                />
+                            </CommandItem>
+                        ))}
+                    </CommandGroup>
+                </Command>
+            </PopoverContent>
+        </Popover>
+    )
 };
 
 export default FileSelector;
