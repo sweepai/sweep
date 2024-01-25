4eb69a55a7e7125dc8fd50ac783f28347f140986
Kevin Lu <kevinlu1248@gmail.com>
2024-01-25 13:50:08 -0800

    Made this npm installable kinda

--- platform/cli.js
+++ platform/cli.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const { spawn } = require('child_process');
 
+const currentDir = process.cwd();
 const sweepaiDir = path.join(process.cwd(), 'node_modules', 'sweepai');
 const envLocalPath = path.join(process.cwd(), '.env.local');
 const targetEnvLocalPath = path.join(sweepaiDir, '.env.local');
@@ -12,7 +13,7 @@ if (fs.existsSync(envLocalPath)) {
   fs.copyFileSync(envLocalPath, targetEnvLocalPath);
 }
 
-const command = process.argv[2] === 'build' ? 'next build --no-lint' : 'next start';
+const command = process.argv[2] === 'build' ? `cd ${sweepaiDir} && npm i && next build --no-lint || ${currentDir}` : `cd ${sweepaiDir} && next start || cd ${currentDir}`;
 const childProcess = spawn('sh', ['-c', command], { cwd: sweepaiDir });
 
 childProcess.stdout.on('data', (data) => {
