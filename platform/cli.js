904e832a9a490ff9edd42a475d95a3fd44204441
Kevin Lu <kevinlu1248@gmail.com>
2024-01-25 13:28:33 -0800

    Fixed cli.js

--- platform/cli.js
+++ platform/cli.js
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
 
+const path = require('path');
 const { spawn } = require('child_process');
 
-const process = spawn('sh', ['-c', 'next start']);
+const sweepaiDir = path.join(process.cwd(), 'node_modules', 'sweepai');
 
-process.stdout.on('data', (data) => {
+const command = process.argv[2] === 'build' ? 'next build' : 'next start';
+const childProcess = spawn('sh', ['-c', command], { cwd: sweepaiDir });
+
+childProcess.stdout.on('data', (data) => {
     console.log(data.toString());
 });
 
-process.stderr.on('data', (data) => {
+childProcess.stderr.on('data', (data) => {
     console.error(data.toString());
 });
 
-process.on('exit', (code) => {
-    console.log(`Child process exited with code ${code}`);
+childProcess.on('exit', (code) => {
+    console.log(`Child childProcess exited with code ${code}`);
 });
