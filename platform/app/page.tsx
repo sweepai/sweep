b813c0ceb1c143e62847b658876b4c7a9e219d6f
Kevin Lu <kevinlu1248@gmail.com>
2024-01-29 09:37:18 -0800

    Platform Planning (#3022)

--- platform/app/page.tsx
+++ platform/app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 
-import DashboardDisplay from "../components/dashboard/DashboardDisplay";
+// import DashboardContext from "@/components/dashboard/DashboardContext";
+import DashboardDisplay from "@/components/dashboard/DashboardDisplay";
 
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <DashboardDisplay></DashboardDisplay>
+      {/* <DashboardContext></DashboardContext> */}
     </main>
   );
 }
