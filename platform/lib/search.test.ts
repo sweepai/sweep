6866d5e0ae5c97b5f26844ffac0e5d49fcc032c7
Kevin Lu <kevinlu1248@gmail.com>
2024-02-02 12:18:09 -0800

    Partially fixed unit-tests

--- platform/lib/search.test.ts
+++ platform/lib/search.test.ts
@@ -1,21 +1,23 @@
 import { listFiles, searchFiles, splitIntoChunks } from "./search";
 
+const { expect } = require("chai");
+
 describe("splitIntoChunks", () => {
   test("splits string into chunks of 40 lines with 30 lines overlapping", () => {
     const input = Array(100).fill("Line").join("\n");
     const result = splitIntoChunks(input);
-    expect(result[0].content.split("\n").length).toBe(40);
-    expect(result[1].content.split("\n").length).toBe(40);
-    expect(result[1].content.split("\n")[0]).toBe("Line");
+    expect(result[0].content.split("\n")).to.have.lengthOf(40);
+    expect(result[1].content.split("\n")).to.have.lengthOf(40);
+    expect(result[1].content.split("\n")[0]).to.equal("Line");
   });
 
   test("handles empty string", () => {
-    expect(splitIntoChunks("")).toEqual([]);
+    expect(splitIntoChunks("")).to.eql([]);
   });
 
   test("handles string with fewer than 40 lines", () => {
     const shortString = Array(20).fill("Line").join("\n");
-    expect(splitIntoChunks(shortString)).toEqual([
+    expect(splitIntoChunks(shortString)).to.eql([
       {
         file: "",
         start: 0,
@@ -29,7 +31,7 @@ describe("splitIntoChunks", () => {
 
 it("fetches all the files", async () => {
   const files = await listFiles("./");
-  expect(files.length > 0);
+  expect(files).to.have.lengthOf.above(0);
 });
 
 it("searched for something", async () => {
@@ -37,5 +39,5 @@ it("searched for something", async () => {
   for (const snippet of snippets) {
     console.log(snippet.file, snippet.content);
   }
-  expect(snippets.length > 0);
+  expect(snippets).to.have.lengthOf.above(0);
 });
