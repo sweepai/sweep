29bd5ed7cd94b9aa8eab30a18a956954be0a6591
Kevin Lu <kevinlu1248@gmail.com>
2024-02-04 19:51:18 -0800

    Bumped version

--- platform/cli.js
+++ platform/cli.js
@@ -4,14 +4,35 @@ const fs = require("fs");
 const path = require("path");
 const { spawn } = require("child_process");
 
-// const currentDir = process.cwd();
-// const envLocalPath = path.join(__dirname, ".env.local");
-// const targetEnvLocalPath = path.join(sweepaiDir, ".env.local");
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-// if (fs.existsSync(envLocalPath)) {
-//   fs.copyFileSync(envLocalPath, targetEnvLocalPath);
-// }
+const envLocalPath = path.join(__dirname, ".env.local");
 
-const command = process.argv[2] === "build" ? `${process.execPath} ${require.resolve('next/dist/bin/next')} build --no-lint` : `${process.execPath} ${require.resolve('next/dist/bin/next')} start --port 3000`;
-console.log(`> ${command}`);
-const childProcess = spawn("sh", ["-c", command], { cwd: __dirname, stdio: "inherit" });
+const main = () => {
+  const command = process.argv[2] === "build" ? `${process.execPath} ${require.resolve('next/dist/bin/next')} build --no-lint` : `${process.execPath} ${require.resolve('next/dist/bin/next')} start --port 3000`;
+  console.log(`> ${command}\n`);
+  spawn("sh", ["-c", command], { cwd: __dirname, stdio: "inherit" });
+}
+
+if (fs.existsSync(envLocalPath)) {
+  const envLocal = fs.readFileSync(envLocalPath, "utf8");
+  if (!envLocal.includes("OPENAI_API_KEY")) {
+    readline.question('Enter your OpenAI API key (https://platform.openai.com/api-keys): ', name => {
+      envLocal += `OPENAI_API_KEY=${name}\n`;
+      fs.writeFileSync(envLocalPath, envLocal);
+      readline.close();
+      main()
+    });
+  } else {
+    main()
+  }
+} else {
+  readline.question('Enter your OpenAI API key (https://platform.openai.com/api-keys): ', name => {
+    fs.writeFileSync(envLocalPath, `OPENAI_API_KEY=${name}\n`);
+    readline.close();
+    main()
+  });
+}
