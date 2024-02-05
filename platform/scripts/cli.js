3208f1ae5c7986c6fae97249566a8ce510817f7d
Kevin Lu <kevinlu1248@gmail.com>
2024-02-05 10:30:00 -0800

    Added inquirer

--- platform/scripts/cli.js
+++ platform/scripts/cli.js
@@ -2,7 +2,6 @@
 
 const fs = require("fs");
 const path = require("path");
-const inquirer = require("inquirer");
 const { spawn } = require("child_process");
 
 const readline = require('readline').createInterface({
@@ -52,28 +51,37 @@ if (!fs.existsSync(envLocalPath)) {
   fs.writeFileSync(envLocalPath, `NEXT_PUBLIC_DEFAULT_REPO_PATH=${process.cwd()}\n`);
 }
 
-const main = () => {
-  const command = process.argv[2] === "build" ? `${process.execPath} ${require.resolve('next/dist/bin/next')} build --no-lint` : `${process.execPath} ${require.resolve('next/dist/bin/next')} start --port 3000`;
-  console.log(`> ${command}\n`);
-  const childProcess = spawn("sh", ["-c", command], { cwd: packagePath, stdio: "inherit" });
+import('inquirer').then((inquirer) => {
+  const main = () => {
+    const command = process.argv[2] === "build" ? `${process.execPath} ${require.resolve('next/dist/bin/next')} build --no-lint` : `${process.execPath} ${require.resolve('next/dist/bin/next')} start --port 3000`;
+    console.log(`> ${command}\n`);
+    const childProcess = spawn("sh", ["-c", command], { cwd: packagePath, stdio: "inherit" });
 
-  // Check the exit code of the process
-  childProcess.on('exit', (code, signal) => {
-    if (code) {
-      console.error(`If you got a message regarding a missing build, try running \`npx sweepai build\` to rebuild the package.`)
-      process.exit(code);
-    }
-  });
-}
+    // Check the exit code of the process
+    childProcess.on('exit', (code, signal) => {
+      if (code) {
+        console.error(`If you got a message regarding a missing build, try running \`npx sweepai build\` to rebuild the package.`)
+        process.exit(code);
+      }
+    });
+  }
 
-var envLocal = fs.readFileSync(envLocalPath, "utf8");
-if (!envLocal.includes("OPENAI_API_KEY")) {
-  readline.question('Enter your OpenAI API key (https://platform.openai.com/api-keys): ', name => {
-    envLocal += `OPENAI_API_KEY=${name}\n`;
-    fs.writeFileSync(envLocalPath, envLocal);
-    readline.close();
+  var envLocal = fs.readFileSync(envLocalPath, "utf8");
+  if (!envLocal.includes("OPENAI_API_KEY")) {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'openai_api_key',
+        message: 'Enter your OpenAI API key (https://platform.openai.com/api-keys):'
+      }
+    ]).then(answers => {
+      envLocal += `OPENAI_API_KEY=${answers.openai_api_key}\n`;
+      fs.writeFileSync(envLocalPath, envLocal);
+      main();
+    }).catch(error => {
+      console.error("An error occurred:", error);
+    });
+  } else {
     main()
-  });
-} else {
-  main()
-}
+  }
+})
