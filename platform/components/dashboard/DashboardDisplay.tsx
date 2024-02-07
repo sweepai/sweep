3185be53bce7fea68b369829294bebeaa9c882f0
Kevin Lu <kevinlu1248@gmail.com>
2024-02-07 10:27:01 -0800

    Linting error

--- platform/components/dashboard/DashboardDisplay.tsx
+++ platform/components/dashboard/DashboardDisplay.tsx
@@ -353,7 +353,6 @@ const DashboardDisplay = () => {
                 hideMerge={hideMerge}
                 oldFile={oldFile}
                 setOldFile={setOldFile}
-                setHideMerge={setHideMerge}
               ></FileSelector>
             </ResizablePanel>
             <ResizableHandle withHandle />
