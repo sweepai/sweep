432e1824d91c98d1a66846ddcfa27baecf060621
wwzeng1 <william@sweep.dev>
2024-02-23 06:57:15 +0000

    add rcm back

--- sweepai/utils/ticket_utils.py
+++ sweepai/utils/ticket_utils.py
@@ -117,12 +117,12 @@ def fetch_relevant_files(
         ticket_progress.search_progress.repo_tree = str(repo_context_manager.dir_obj)
         ticket_progress.save()
 
-        # repo_context_manager = get_relevant_context(
-        #     formatted_query,
-        #     repo_context_manager,
-        #     ticket_progress,
-        #     chat_logger=chat_logger,
-        # )
+        repo_context_manager = get_relevant_context(
+            formatted_query,
+            repo_context_manager,
+            ticket_progress,
+            chat_logger=chat_logger,
+        )
         snippets = repo_context_manager.current_top_snippets
         ticket_progress.search_progress.repo_tree = str(repo_context_manager.dir_obj)
         ticket_progress.search_progress.final_snippets = snippets
