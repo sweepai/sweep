911a0e74e4a5cc934f2a4a858e5f725aa76160b2
Kevin Lu <kevinlu1248@gmail.com>
2024-02-26 00:02:58 -0800

    Assistant wrapper bugs

--- sweepai/agents/assistant_wrapper.py
+++ sweepai/agents/assistant_wrapper.py
@@ -275,18 +275,22 @@ def run_until_complete(
                     # OpenAI has a bug where it calls the imaginary function "multi_tool_use.parallel"
                     # Based on https://github.com/phdowling/openai_multi_tool_use_parallel_patch/blob/main/openai_multi_tool_use_parallel_patch.py
                     if tool_function_name in ("multi_tool_use.parallel", "parallel"):
-                        for fake_i, fake_tool_uses in function_input["tool_uses"]:
-                            for fake_tool_use in fake_tool_uses:
-                                function_input = fake_tool_use["parameters"]
-                                function_name: str = fake_tool_use["recipient_name"]
-                                function_name = function_name.removeprefix("functions.")
-                                tool_calls.append(
-                                    (
-                                        f"{tool_call.id}_{fake_i}",
-                                        function_name,
-                                        function_input,
-                                    )
+                        # import pdb
+
+                        # pdb.set_trace()
+                        # print(tool_function_input)
+                        # print(function_input)
+                        for fake_i, fake_tool_use in function_input["tool_uses"]:
+                            function_input = fake_tool_use["parameters"]
+                            function_name: str = fake_tool_use["recipient_name"]
+                            function_name = function_name.removeprefix("functions.")
+                            tool_calls.append(
+                                (
+                                    f"{tool_call.id}_{fake_i}",
+                                    function_name,
+                                    function_input,
                                 )
+                            )
                     else:
                         tool_calls.append(
                             (tool_call.id, tool_function_name, tool_function_input)
