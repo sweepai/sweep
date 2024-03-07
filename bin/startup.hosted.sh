d34d19e844f4de7427d79dd153e5f7460cec7030
Kevin Lu <kevinlu1248@gmail.com>
2024-03-07 12:47:17 -0800

    Sweep self-hosted

--- bin/startup.hosted.sh
+++ bin/startup.hosted.sh
@@ -1,5 +1,49 @@
 #!/bin/sh
 
-echo PORT: ${PORT:-8080}
-redis-server /app/redis.conf --bind 0.0.0.0 --port 6379 &
+RED="\033[0;31m"
+GREEN="\033[1;32m"
+BLUE="\033[1;34m"
+BLUE="\033[1;35m"
+YELLOW="\033[1;33m"
+BOLD="\033[1m"
+NC="\033[0m" # No Color
+
+echo -e "
+
+                         @@@@@@@@@@@@@@@@@@@%*
+                     #@@@                    @+
+                   @@                       @
+                 =@             @@@       @@
+                @@              @  @=   @-
+                @               @@ @*   @
+               @                +@ @%   @
+               %@               *@ @=   @@
+               @@@@@            =% %  @@
+              @@@@@@@@@@@@@@@@@@@  @@@@@
+              @@@@@@@@@@@@@@@@@@@  @@@@@@
+             @@@@@@@@@@@@@@@@@@@@  @@@@@@#                  ${BLUE}Sweep AI Assistant${NC}
+             @@@   %@@@   @@@@@@  @@@@@@@@@
+             @@@    @@%   *@@@@# @@@@@@@@@@@@@@
+             @@@   @@@@   @@@@@ @@@@@@@@@@@@@@
+             @@@@@@@@@@@@@@@@@  @@@@@@@@@@@@@               https://docs.sweep.dev/assistant
+              @@@@@@@@@@@@@@@@  @@@@@@@@@@@@
+               @@@@@@@@@@@@@@  @@@@@@@@@@@@
+                #@@@@@@@@@@@    @@@@@@@@@@
+                   @@@@@@@    @@@@@@@@@*
+                      @*    @@@@@@@@
+           @%*@@@@@@@       %
+           #@               #@
+             @            @ @@
+              @@         @# @*
+                @@*      @  @
+                  %@@@# @@  @
+                      %@@@@@
+
+                                                            "
+
+echo -e "${YELLOW}You're currently using the free version of self-hosted Sweep AI. For more performance, like fine-tuned search, switch to our enterprise version. Email us at ${BLUE}team@sweep.dev${YELLOW} or schedule a call at ${BLUE}https://calendly.com/sweep-ai/founders-meeting${NC}.\n"
+
+
+echo -e "${YELLOW}Launching sweep on https://localhost:${PORT:-8080}${NC}"
+redis-server /app/redis.conf --bind 0.0.0.0 --port 6379 > /dev/null 2>&1 &
 uvicorn sweepai.api:app --host 0.0.0.0 --port ${PORT:-8080} --workers 30
