657f3fb8335985911015a81d489384ab9c9d02b7
Kevin Lu <kevinlu1248@gmail.com>
2024-03-12 22:48:41 -0700

    Coloring improvements

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -7,7 +7,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         // <main className="bg-[#020817]" style={{backgroundColor: "#020817"}}>
         // <main className="bg-[#020817]" style={{backgroundColor: "#0d0a19"}}>
         // <main className="bg-[#020817]" style={{backgroundColor: "#161625"}}>
-        <main className="bg-[#020817]" style={{backgroundColor: "#13131A"}}>
+        // <main className="bg-[#020817]" style={{backgroundColor: "#13131A"}}>
+        // <main className="bg-[#020817]" style={{backgroundColor: "#121221"}}>
+        // <main style={{backgroundColor: "#10101E"}}>
+        <main style={{backgroundColor: "#0E0E1C"}}>
           <Component {...pageProps} />
         </main>
     );
