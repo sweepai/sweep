f04525bf4b6df62ee9ed66453dd93d8e932d99f2
Kevin Lu <kevinlu1248@gmail.com>
2024-03-24 18:53:58 -0700

    Update on_ticket.py

--- sweepai/handlers/on_ticket.py
+++ sweepai/handlers/on_ticket.py
@@ -470,8 +470,6 @@ def on_ticket(
             branch_match = re.search(
                 r"([B|b]ranch:) *(?P<branch_name>.+?)(\s|$)", summary
             )
-            if username.endswith("[bot]"):
-                summary = distill_issue(summary)
             overrided_branch_name = None
             if branch_match and "branch_name" in branch_match.groupdict():
                 overrided_branch_name = (
