a9d2cb4890f66a7d3031a3d1f018019531fdf2b2
Kevin Lu <kevinlu1248@gmail.com>
2024-03-30 07:02:55 +0000

    Minor fix

--- sweepai/config/server.py
+++ sweepai/config/server.py
@@ -30,9 +30,6 @@ SENTENCE_TRANSFORMERS_MODEL = os.environ.get(
     "SENTENCE_TRANSFORMERS_MODEL",
     "sentence-transformers/all-MiniLM-L6-v2",  # "all-mpnet-base-v2"
 )
-BATCH_SIZE = int(
-    os.environ.get("BATCH_SIZE", 48) # Voyage only allows 128 items per batch and 120000 tokens per batch
-)
 
 TEST_BOT_NAME = "sweep-nightly[bot]"
 ENV = os.environ.get("ENV", "dev")
@@ -229,3 +226,7 @@ USE_ASSISTANT = os.environ.get("USE_ASSISTANT", "true").lower() == "true"
 ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", None)
 VOYAGE_API_KEY = os.environ.get("VOYAGE_API_KEY", None)
 PAREA_API_KEY = os.environ.get("PAREA_API_KEY", None)
+
+BATCH_SIZE = int(
+    os.environ.get("BATCH_SIZE", 48 if VOYAGE_API_KEY else 256) # Voyage only allows 128 items per batch and 120000 tokens per batch
+)
