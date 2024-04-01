36f1e0753acfa976eefc7c7cb10dcb9046d5192c
Kevin Lu <kevinlu1248@gmail.com>
2024-04-01 22:35:29 +0000

    Sweep bot minor bug fix

--- sweepai/core/sweep_bot.py
+++ sweepai/core/sweep_bot.py
@@ -723,7 +723,7 @@ class SweepBot(CodeGenBot, GithubBot):
                 ]
                 # add context on which fcr it is currently on and how many are left
                 all_fcrs_strings = ", ".join(file_change_request.relevant_files)
-                current_fcr_strings = f"To solve the user's request you will modify/create the following {len(file_change_request.relevant_files)} files: {all_fcrs_strings}. You are currently modifying {file_change_request.relevant_files[i]}!"
+                current_fcr_strings = f"To solve the user's request you will modify/create the following {len(file_change_request.relevant_files)} files: {all_fcrs_strings}. You are currently modifying {file_change_request.filename}!"
                 additional_messages += [
                     Message(
                         role="user",
