afb35eb84eddce069d1d383c8abfe8dd104870ab
wwzeng1 <william@sweep.dev>
2024-04-19 00:04:17 +0000

    fix ruff

--- sweepai/core/sweep_bot.py
+++ sweepai/core/sweep_bot.py
@@ -36,7 +36,6 @@ from sweepai.core.prompts import (
     files_to_change_system_prompt
 )
 from sweepai.utils.chat_logger import ChatLogger, discord_log_error
-from sweepai.utils.previous_diff_utils import get_previous_diffs
 from sweepai.utils.progress import (
     AssistantAPIMessage,
     AssistantConversation,
@@ -268,11 +267,11 @@ def get_files_to_change(
                 key="graph_text",
             )
         )
-    previous_diffs = get_previous_diffs(
-        problem_statement,
-        cloned_repo=cloned_repo,
-        relevant_file_paths=[snippet.file_path for snippet in relevant_snippets],
-    )
+    # previous_diffs = get_previous_diffs(
+    #     problem_statement,
+    #     cloned_repo=cloned_repo,
+    #     relevant_file_paths=[snippet.file_path for snippet in relevant_snippets],
+    # )
     # messages.append( # temporarily disable in main
     #     Message(
     #         role="user",
