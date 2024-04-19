a99f12a34446c5d3e51a6b54c9de17687928fca7
Kevin Lu <kevinlu1248@gmail.com>
2024-04-19 22:46:29 +0000

    Bug fix

--- sweepai/utils/ticket_utils.py
+++ sweepai/utils/ticket_utils.py
@@ -16,6 +16,7 @@ from sweepai.core.lexical_search import (
     search_index,
 )
 from sweepai.core.sweep_bot import get_files_to_change
+from sweepai.logn.cache import file_cache
 from sweepai.utils.chat_logger import discord_log_error
 from sweepai.utils.cohere_utils import cohere_rerank_call
 from sweepai.utils.event_logger import posthog
@@ -99,7 +100,7 @@ def apply_adjustment_score(
 
 NUM_SNIPPETS_TO_RERANK = 100
 
-# @file_cache()
+@file_cache()
 def multi_get_top_k_snippets(
     cloned_repo: ClonedRepo,
     queries: list[str],
@@ -156,7 +157,7 @@ def multi_get_top_k_snippets(
     ]
     return ranked_snippets_list, snippets, content_to_lexical_score_list
 
-# @file_cache()
+@file_cache()
 def get_top_k_snippets(
     cloned_repo: ClonedRepo,
     query: str,
