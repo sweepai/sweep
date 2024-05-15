2922de61f473e31321e08c0d71de9f3feed9fafa
MartinYe1234 <52641447+MartinYe1234@users.noreply.github.com>
2024-05-15 16:04:32 -0700

    Feat/review pr redundant (#3755)
    improvements, made it more reliable and produce fewer issues overall
    
    ---------
    
    Co-authored-by: Martin Ye <martin@sweep.dev>

--- sweepai/core/review_utils.py
+++ sweepai/core/review_utils.py
@@ -1,7 +1,6 @@
 """
 Take a PR and provide an AI generated review of the PR.
 """
-from dataclasses import dataclass
 import re
 
 from tqdm import tqdm
@@ -9,6 +8,7 @@ from sweepai.core.chat import ChatGPT
 from sweepai.core.entities import Message
 from sweepai.core.review_annotations import get_diff_annotations
 from sweepai.core.sweep_bot import safe_decode
+from sweepai.dataclasses.codereview import CodeReview, PRChange, Patch
 from sweepai.logn.cache import file_cache
 from sweepai.utils.event_logger import logger
 from sweepai.utils.chat_logger import ChatLogger
@@ -16,7 +16,8 @@ from github.GithubException import GithubException
 from github.Repository import Repository
 from github.PullRequest import PullRequest
 
-from sweepai.utils.str_utils import add_line_numbers
+from sweepai.utils.str_utils import add_line_numbers, objects_to_xml
+from sweepai.utils.ticket_rendering_utils import parse_issues_from_code_review
 
 def get_pr_diffs(repo: Repository, pr: PullRequest):
     base_sha = pr.base.sha
@@ -43,24 +44,6 @@ def get_pr_diffs(repo: Repository, pr: PullRequest):
             f"File status {file.status} not recognized"
             )
     return pr_diffs
-
-@dataclass
-class Patch:
-    old_start: int
-    old_count: int
-    new_start: int
-    new_count: int
-    changes: str
-
-@dataclass
-class PRChange:
-    file_name: str
-    diff: str
-    old_code: str
-    new_code: str
-    status: str
-    patches: list[Patch]
-    annotations: list[str] = None
 
 @file_cache()
 def get_pr_changes(repo: Repository, pr: PullRequest) -> list[PRChange]:
@@ -132,6 +115,13 @@ pr_change_unformatted = """\
 <file_patches>
 {patches}
 </file_patches>
+</pr_change>"""
+
+pr_change_with_source_code_unformatted = """\
+<pr_change file_name="{file_name}">
+<file_patches>
+{patches}
+</file_patches>
 
 And here is the full source code after applying the pull request changes:
 <source_code>
@@ -147,16 +137,23 @@ patch_format = """\
 {annotation}
 </patch_annotation>"""
 
-def format_pr_change(pr_change: PRChange, pr_idx: int=0):
+# format only the patches for the PRChange
+def format_patches_for_pr_change(pr_change: PRChange):
     patches = ""
     for idx, patch in enumerate(pr_change.patches):
-        patches += "\n" + patch_format.format(
+        patches += patch_format.format(
             file_name=pr_change.file_name,
             index=idx + 1,
             diff=patch.changes,
             annotation=pr_change.annotations[idx]
         )
-    return pr_change_unformatted.format(
+        if idx < len(pr_change.patches) - 1:
+            patches += "\n"
+    return patches
+
+def format_pr_change(pr_change: PRChange, pr_idx: int=0):
+    patches = format_patches_for_pr_change(pr_change)
+    return pr_change_with_source_code_unformatted.format(
         file_name=pr_change.file_name,
         patches=patches,
         file_contents=add_line_numbers(pr_change.new_code, start=1)
@@ -169,6 +166,9 @@ def format_pr_changes_by_file(pr_changes: list[PRChange]):
     return formatted_pr_changes_by_file
 
 system_prompt = """You are a careful and smart tech lead that wants to avoid production issues. You will be analyzing a set of diffs representing a pull request made to a piece of source code. Be very concise."""
+
+system_prompt_review = """You are a busy tech manager who is responsible for reviewing prs and identifying any possible production issues. 
+You will be analyzing a list of potential issues that have been identified by a previous engineer and determing which issues are severe enough to bring up to the original engineer."""
 
 user_prompt = """\
 # Code Review
@@ -188,11 +188,11 @@ Here are the changes in the pull request diffs:
     1c. Describe the key changes that were made in the diffs. (1 paragraph)
 <thoughts>
 <thinking>
-{{Analysis of changes}}
+{{Analysis of diff/patch 1}}
 </thinking>
 ...
 </thoughts>
-    1d. Provide a final summary that should be a single sentence and formatted within a <diff_summary> tag.:
+    1d. Provide a final summary for this file that should be a single sentence and formatted within a <diff_summary> tag.:
 <diff_summary>
 {{Final summary of changes}}
 </diff_summary>
@@ -200,8 +200,9 @@ Here are the changes in the pull request diffs:
 2. Identify all issues.
     2a. Determine whether there are any functional issues, bugs, edge cases, or error conditions that the code changes introduce or fail to properly handle. Consider the line-by-line analysis from step 1b. (1 paragraph)
     2b. Determine whether there are any security vulnerabilities or potential security issues introduced by the code changes. (1 paragraph) 
-    2c. Identify any other potential issues that the code changes may introduce that were not captured by 2a or 2b. This could include accidental changes, commented out code, or other suspicious modifications. (1 paragraph)
-    2d. Format the found issues and root causes using the following XML tags. Each issue description should be a single sentence. Include the corresponding start and end line numbers. Format these fields in an <issue> tag in the following manner:
+    2c. Identify any other potential issues that the code changes may introduce that were not captured by 2a or 2b. This could include accidental changes such as commented out code, or other suspicious modifications. (1 paragraph)
+    2d. Only include issues that you are very confident will cause serious issues that prevent the pull request from being merged. For example, focus only on functional code changes and ignore changes to strings and comments that are purely descriptive.
+    2e. Format the found issues and root causes using the following XML tags. Each issue description should be a single sentence. Include the corresponding start and end line numbers, these line numbers should only include lines of code that have been changed. DO NOT reference the patch or patch number in the description. Format these fields in an <issue> tag in the following manner:
 <issues>
 <issue>
 <issue_description>
@@ -220,15 +221,69 @@ If there are no issues found do not include the corresponding <issue> tag.
 
 Focus your analysis solely on potential functional issues with the code changes. Do not comment on stylistic, formatting, or other more subjective aspects of the code."""
 
+user_prompt_review = """
+Below are a series of identified issues for the file {file_name} formatted in the following way:
+<potential_issues>
+<issue>
+<issue_description>
+{{Issue 1 description}}
+</issue_description>
+<start_line>
+{{Corresponding starting line number for Issue 1}}
+</start_line>
+<end_line>
+{{Corresponding ending line number for Issue 1 (this can be the same as the start_line)}}
+</end_line>
+</issue>
+...
+</potential_issues>
+
+# Potential Issues
+
+{potential_issues}
+
+Below are the changes made in the pull request as context
+# Relevant code files with line numbers
+
+{pr_changes}
+
+# Instructions
+1. Analyze each identified potential issue for the file {file_name}
+    1a. Review each identified issue individually, formulate 3-5 questions to answer in order to determine the severity of the issue.
+    1b. Answer the questions formulated in step 1a. In order to accomplish this examine the referenced lines of code in the provided code files above.
+    1c. Determine whether or not this issue is severe enough to prevent the pull request from being merged or not. For example, any potential logical error is considered severe.
+    1d. Take note of some common issues: Accidently removing or commenting out lines of code that has functional utility. In this case double check if this change was intentional or accidental.
+    1e. Deliver your analysis in the following format:
+<thoughts>
+<thinking>
+{{Analysis of the issue, include the questions and answers}}
+</thinking>
+...
+</thoughts>
+
+2. Decide which issues to keep
+    2a. Based on your analysis in step 1, now decide which issues to keep and drop. Only include severe issues.
+    2b. After choosing to keep an issue you are to respond in the following format:
+<severe_issues>
+<issue>
+<issue_description>
+{{Issue 1 description}}
+</issue_description>
+<start_line>
+{{Corresponding starting line number for Issue 1}}
+</start_line>
+<end_line>
+{{Corresponding ending line number for Issue 1}}
+</end_line>
+</issue>
+...
+</severe_issues>
+"""
+
 CLAUDE_MODEL = "claude-3-opus-20240229"
 
-@dataclass
-class CodeReview:
-    file_name: str
-    diff_summary: str
-    issues: str
-
 class PRReviewBot(ChatGPT):
+    # fetch all potential issues for each file based on the diffs of that file
     def review_code_changes_by_file(self, pr_changes_by_file: dict[str, str], chat_logger: ChatLogger = None):
         code_reviews_by_file = {}
         for file_name, pr_changes in pr_changes_by_file.items():
@@ -241,7 +296,7 @@ class PRReviewBot(ChatGPT):
             formatted_user_prompt = user_prompt.format(diff=pr_changes)
             code_review_response = self.chat_anthropic(
                 content=formatted_user_prompt,
-                temperature=0.2,
+                temperature=0,
                 model=CLAUDE_MODEL,
                 use_openai=True
             )
@@ -256,7 +311,60 @@ class PRReviewBot(ChatGPT):
             issues_matches = re.findall(issues_pattern, code_review_response, re.DOTALL)
             if issues_matches:
                 issues = "\n".join([match.strip() for match in issues_matches])
-            code_reviews_by_file[file_name] = CodeReview(file_name=file_name, diff_summary=diff_summary, issues=issues)
+            potential_issues = parse_issues_from_code_review(issues)
+            code_reviews_by_file[file_name] = CodeReview(file_name=file_name, diff_summary=diff_summary, issues=potential_issues)
+            if chat_logger:
+                chat_logger.add_chat(
+                    {
+                        "model": self.model,
+                        "messages": [{"role": message.role, "content": message.content} for message in self.messages],
+                        "output": "END OF MESSAGES",
+                    })
+        return code_reviews_by_file
+
+    # review the generated issues more critically for each file to see if they are actually important or not
+    def review_code_issues_by_file(self, pr_changes: list[PRChange], formatted_pr_changes_by_file: dict[str, str], code_reviews_by_file: dict[str, CodeReview], chat_logger: ChatLogger = None):
+        files_to_patches = {}
+        # format all patches for all files
+        for pr_change in pr_changes:
+            patches = format_patches_for_pr_change(pr_change)
+            files_to_patches[pr_change.file_name] = patches
+
+        # go file by file
+        for file_name, code_review in code_reviews_by_file.items():
+            self.messages = [
+                Message(
+                    role="system",
+                    content=system_prompt_review,
+                )
+            ]
+            # if no issues were identified continue to next file
+            if not code_review.issues:
+                continue
+            # convert our CodeReviewIssue list to an xml string
+            potential_issues_string = objects_to_xml(code_review.issues, "issue", outer_field_name="potential_issues")
+            # now prepend all other pr changes to the current pr change
+            all_other_pr_changes = "\n\n".join([pr_change_unformatted.format(file_name=file, patches=patches) for file, patches in files_to_patches.items() if file != file_name])
+            
+            formatted_user_prompt = user_prompt_review.format(file_name=file_name, potential_issues=potential_issues_string, pr_changes=f"{all_other_pr_changes}\n{formatted_pr_changes_by_file[file_name]}")
+            code_review_response = self.chat_anthropic(
+                content=formatted_user_prompt,
+                temperature=0,
+                model=CLAUDE_MODEL,
+                use_openai=True
+            )
+
+            severe_issues_pattern = r"<severe_issues>(.*?)</severe_issues>"
+            issues_matches = re.findall(severe_issues_pattern, code_review_response, re.DOTALL)
+            if issues_matches:
+                issues = "\n".join([match.strip() for match in issues_matches])
+                potential_issues = parse_issues_from_code_review(issues)
+            else:
+                potential_issues = []
+            
+            # update the issues
+            code_reviews_by_file[file_name].issues = potential_issues
+            
             if chat_logger:
                 chat_logger.add_chat(
                     {
