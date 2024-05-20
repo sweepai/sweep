7f2c8d460d982c04d842d6b149c0f6bc46174c7d
Kevin Lu <kevinlu1248@gmail.com>
2024-05-20 16:47:47 +0000

    Fixed

--- sweepai/handlers/on_ticket.py
+++ sweepai/handlers/on_ticket.py
@@ -239,10 +239,8 @@ def on_ticket(
                 " time using Sweep, I'm indexing your repository, which will take a few minutes."
             )
             first_comment = (
-                f"{get_comment_header(0, g, repo_full_name, progress_headers, tracking_id, payment_message_start)}\n{sep}I am currently looking into this ticket! I"
-                " will update the progress of the ticket in this comment. I am currently"
-                f" searching through your code, looking for relevant snippets.\n{sep}##"
-                f" {progress_headers[1]}\n{indexing_message}{bot_suffix}{discord_suffix}"
+                f"{get_comment_header(0, g, repo_full_name, progress_headers, tracking_id, payment_message_start)}\n## "
+                f"{progress_headers[1]}\n{indexing_message}{bot_suffix}{discord_suffix}"
             )
             # Find Sweep's previous comment
             comments = []
