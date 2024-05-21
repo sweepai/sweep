6a793b3e2ac9c306f08a7e4a6905ada820bbe918
Martin Ye <martin@sweep.dev>
2024-05-21 22:54:42 +0000

    added exception

--- sweepai/utils/file_utils.py
+++ sweepai/utils/file_utils.py
@@ -1,12 +1,18 @@
 def read_file_with_fallback_encodings(
     file_path, encodings=["utf-8", "windows-1252", "iso-8859-1"]
 ):
+    embedded_null_byte = False
     for encoding in encodings:
         try:
             with open(file_path, "r", encoding=encoding) as file:
                 return file.read()
         except UnicodeDecodeError:
             continue
+        except ValueError:
+            embedded_null_byte = True
+            continue
+    if embedded_null_byte:
+        raise Exception(f"Encountered null byte while decoding {file_path}")
     raise UnicodeDecodeError(
         f"Could not decode {file_path} with any of the specified encodings: {encodings}"
     )
