1fed29055066727831a2270a397d1425c3f4d5f1
Kevin Lu <kevinlu1248@gmail.com>
2024-05-23 19:58:28 +0000

    More fixes

--- sweepai/agents/modify_utils.py
+++ sweepai/agents/modify_utils.py
@@ -15,7 +15,7 @@ from sweepai.utils.convert_openai_anthropic import AnthropicFunctionCall
 from sweepai.utils.diff import generate_diff
 from sweepai.utils.github_utils import ClonedRepo
 from sweepai.utils.ripgrep_utils import manual_code_check
-from sweepai.utils.tiktoken_utils import get_check_results
+from sweepai.utils.code_validators import get_check_results
 
 modify_tools = """
 # make_change - Make a SINGLE, TARGETED code change in a file. Preserve whitespace, comments, and style. Changes should be minimal, self-contained, and address only one specific modification. If a change affects multiple separate code sections, use multiple calls to this tool, one for each section.
