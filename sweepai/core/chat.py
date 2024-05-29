4451f417d754ef037a0eb9de3b878a88a0fab4cf
Kevin Lu <kevinlu1248@gmail.com>
2024-05-29 04:40:26 +0000

    Chat continuation experiments

--- sweepai/core/chat.py
+++ sweepai/core/chat.py
@@ -674,17 +674,21 @@ def continuous_llm_calls(
         last_line_index = response.rfind("\n")
         content = ""
         if use_openai:
-            content = "Continue from your last message."
+            # content = "Continue from your last message."
             last_block_original_code = response.rfind("<original_code>\n") + len("<original_code>\n")
             last_block_new_code = response.rfind("<new_code>\n") + len("<new_code>\n")
-            if last_block_new_code < last_block_original_code:
-                content = "Continue from your last message, continue generating the original_code."
-            elif last_block_original_code < last_block_new_code:
-                content = "Continue from your last message, generating the new_code."
+            # if last_block_new_code < last_block_original_code:
+            #     content = "Continue from your last message, continue generating the original_code."
+            # elif last_block_original_code < last_block_new_code:
+            #     content = "Continue from your last message, generating the new_code."
             last_block = max(last_block_original_code, last_block_new_code)
             if last_line_index - last_block < 2500:
                 last_line_index = last_block
         response = response[:last_line_index].rstrip()
+        last_k_lines = response.split("\n")[-5:]
+        if use_openai:
+            content = "Continue generating from here:\n" + "\n".join(last_k_lines)
+            breakpoint()
         chat_gpt.messages[-1].content = response_cleanup(response)
         # ask for a second response
         try:
