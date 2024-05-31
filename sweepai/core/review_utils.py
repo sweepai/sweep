2509cc8134398ac1897d8378a119ce57920ce420
Martin Ye <martin@sweep.dev>
2024-05-31 20:36:37 +0000

    fixed index check

--- sweepai/core/review_utils.py
+++ sweepai/core/review_utils.py
@@ -1053,8 +1053,8 @@ class PRReviewBot(ChatGPT):
                 issue_index += 1
         all_issues_formatted += "\n</all_issues>"
 
-        # if there was only one issue there is no need to sort anything
-        if issue_index == 2:
+        # if there was only one minus issue there is no need to sort anything
+        if issue_index <= 2:
             return code_review_by_file, []
         
         self.messages = [
