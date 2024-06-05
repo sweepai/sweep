4a59c8c766a1acb3aee8518ceaaf16875afd8b32
Martin Ye <martin@sweep.dev>
2024-06-05 19:13:22 +0000

    updated and improved test

--- tests/e2e/test_pr_review.py
+++ tests/e2e/test_pr_review.py
@@ -1,71 +1,26 @@
-import datetime
-import json
-import os
 import sys
-import time
 import traceback
 
-from fastapi.testclient import TestClient
-from github import Github
-
-from sweepai.api import app, global_threads
-
-GITHUB_PAT = os.environ["GITHUB_PAT"]
-print(
-    f"Using GITHUB_PAT: "
-    + GITHUB_PAT[:3]
-    + "*" * (len(GITHUB_PAT) - 4)
-    + GITHUB_PAT[-1:]
-)
-g = Github(GITHUB_PAT)
-repo_name = "sweepai/e2e"  # for e2e test this is hardcoded
-repo = g.get_repo(repo_name)
-
-local_tz = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
-# PR NUMBER is hardcoded for e2e test
-pr_number = 1349
+from sweepai.api import global_threads
+from sweepai.handlers.review_pr import review_pr
+from sweepai.utils.github_utils import get_github_client, get_installation_id
 
 
 def test_e2e_pr_review():
-    client = TestClient(app)
     try:
-        issue_json = json.load(open("tests/jsons/e2e_pr_review.json", "r"))
-        start_time = time.time()
-        response = client.post(
-            "/",
-            json=issue_json,
-            headers={"X-GitHub-Event": "pull_request"},
-        )
-        print(f"Completed in {time.time() - start_time}s")
-        assert response
-        assert response.text
-        response_text = json.loads(response.text)
-        assert response.status_code == 200
-        assert "success" in response_text
+        repo_name = "sweepai/e2e"  # for e2e test this is hardcoded
+        installation_id = get_installation_id("sweepai")
+        pr_number = 1349
+        _user_token, g = get_github_client(installation_id)
+        repo = g.get_repo(repo_name)
         pr = repo.get_pull(pr_number)
-        # poll github 20 times, waiting 1 minute between each poll, check if the pr has been updated or not
-        for i in range(20):
-            pr = repo.get_pull(pr_number)
-            # iterate through the comments of the pr and check if a new comment got created the title Sweep: PR Review
-            comments = pr.get_issue_comments()
-            for comment in comments:
-                # success if a new pr was made within i+1 minutes ago
-                if (
-                    "Sweep: PR Review" in comment.body
-                ):
-                    i = 1
-                    for thread in global_threads:
-                        thread.join()
-                        print(f"joining thread {i} of {len(global_threads)}")
-                        i += 1
-                    print(f"PR successfully updated: {pr.title}")
-                    print(f"PR object is: {pr}")
-                    # delete comment for next time around
-                    comment.delete()
-                    return
-                    
-            time.sleep(60)
-        raise AssertionError("PR was not updated!")
+        
+        review_pr(
+            "E2E-test-user",
+            pr,
+            repo,
+            installation_id,
+        )
     except AssertionError as e:
         for thread in global_threads:
             thread.join()
