54ccc9bab5ac9d659c5e6949b4b58c0fd203c227
Kevin Lu <kevinlu1248@gmail.com>
2024-06-20 13:19:22 -0700

    Prettier

--- sweep_chat/cypress/e2e/spec.cy.ts
+++ sweep_chat/cypress/e2e/spec.cy.ts
@@ -1,85 +1,85 @@
 const testMessage =
-  "In the vector search logic, how would I migrate the KNN to use HNSW instead?"
+  'In the vector search logic, how would I migrate the KNN to use HNSW instead?'
 const testPullRequestMessage =
-  "Help me review this PR: https://github.com/sweepai/sweep/pull/3978"
+  'Help me review this PR: https://github.com/sweepai/sweep/pull/3978'
 
-describe("sweep chat", () => {
+describe('sweep chat', () => {
   beforeEach(() => {
     cy.login()
-    cy.visit("/")
+    cy.visit('/')
   })
 
-  it("should provide a valid session", () => {
-    cy.get(".rounded-full").click()
-    cy.get("div:nth-child(4)").contains("Sign Out")
+  it('should provide a valid session', () => {
+    cy.get('.rounded-full').click()
+    cy.get('div:nth-child(4)').contains('Sign Out')
   })
 
-  it("can set a repo", () => {
+  it('can set a repo', () => {
     cy.get('[tabindex="-1"] > :nth-child(2) > .items-center > input')
-      .type("sweepai/sweep")
+      .type('sweepai/sweep')
       .blur()
-    cy.get(".h-screen > :nth-child(4) > .flex", { timeout: 10000 }).should(
-      "have.attr",
-      "placeholder",
-      "Type a message..."
+    cy.get('.h-screen > :nth-child(4) > .flex', { timeout: 10000 }).should(
+      'have.attr',
+      'placeholder',
+      'Type a message...'
     )
   })
 
-  it("can stop the chat", () => {
+  it('can stop the chat', () => {
     cy.get('[tabindex="-1"] > :nth-child(2) > .items-center > input')
-      .type("sweepai/sweep")
+      .type('sweepai/sweep')
       .blur()
-    cy.get(".h-screen > :nth-child(4) > .flex", { timeout: 10000 }).should(
-      "have.attr",
-      "placeholder",
-      "Type a message..."
+    cy.get('.h-screen > :nth-child(4) > .flex', { timeout: 10000 }).should(
+      'have.attr',
+      'placeholder',
+      'Type a message...'
     )
 
-    cy.get(".h-screen > :nth-child(4) > .flex").type(testMessage + "{enter}")
+    cy.get('.h-screen > :nth-child(4) > .flex').type(testMessage + '{enter}')
     cy.wait(1000)
-    cy.get(".bg-destructive").click()
-    cy.on("uncaught:exception", (err, runnable) => {
-      expect(err.message).to.include("No snippets found")
+    cy.get('.bg-destructive').click()
+    cy.on('uncaught:exception', (err, runnable) => {
+      expect(err.message).to.include('No snippets found')
       return false
     })
   })
 
-  it("can preview pull requests", () => {
+  it('can preview pull requests', () => {
     cy.get('[tabindex="-1"] > :nth-child(2) > .items-center > input')
-      .type("sweepai/sweep")
+      .type('sweepai/sweep')
       .blur()
-    cy.get(".h-screen > :nth-child(4) > .flex", { timeout: 10000 }).should(
-      "have.attr",
-      "placeholder",
-      "Type a message..."
+    cy.get('.h-screen > :nth-child(4) > .flex', { timeout: 10000 }).should(
+      'have.attr',
+      'placeholder',
+      'Type a message...'
     )
 
-    cy.get(".h-screen > :nth-child(4) > .flex").type(
-      testPullRequestMessage + "{enter}"
+    cy.get('.h-screen > :nth-child(4) > .flex').type(
+      testPullRequestMessage + '{enter}'
     )
-    cy.get("a > .bg-zinc-800").contains("Minor ticket utils fix")
+    cy.get('a > .bg-zinc-800').contains('Minor ticket utils fix')
   })
 
-  it("can send a message", () => {
+  it('can send a message', () => {
     cy.get('[tabindex="-1"] > :nth-child(2) > .items-center > input')
-      .type("sweepai/sweep")
+      .type('sweepai/sweep')
       .blur()
-    cy.get(".h-screen > :nth-child(4) > .flex", { timeout: 10000 }).should(
-      "have.attr",
-      "placeholder",
-      "Type a message..."
+    cy.get('.h-screen > :nth-child(4) > .flex', { timeout: 10000 }).should(
+      'have.attr',
+      'placeholder',
+      'Type a message...'
     )
 
-    cy.get(".h-screen > :nth-child(4) > .flex").type(testMessage + "{enter}")
-    cy.get(".justify-end > .transition-color").should(
-      "contain.text",
+    cy.get('.h-screen > :nth-child(4) > .flex').type(testMessage + '{enter}')
+    cy.get('.justify-end > .transition-color').should(
+      'contain.text',
       testMessage
     )
 
     // Validate response from the LLM
-    cy.get(":nth-child(3) > .transition-color", { timeout: 30000 }).should(
-      "contain.text",
-      "Analysis"
+    cy.get(':nth-child(3) > .transition-color', { timeout: 30000 }).should(
+      'contain.text',
+      'Analysis'
     )
     cy.url().then((url) => {
       expect(url.length).to.be.greaterThan(50)
--- sweep_chat/cypress/support/commands.ts
+++ sweep_chat/cypress/support/commands.ts
@@ -36,7 +36,7 @@
 //   }
 // }
 
-declare module "cypress" {
+declare module 'cypress' {
   namespace Cypress {
     interface Chainable {
       /**
@@ -47,26 +47,26 @@ declare module "cypress" {
   }
 }
 
-const loginSecureCookie = Cypress.env("LOGIN_SECURE_COOKIE")
-const loginSessionCookie = Cypress.env("LOGIN_SESSION_COOKIE")
+const loginSecureCookie = Cypress.env('LOGIN_SECURE_COOKIE')
+const loginSessionCookie = Cypress.env('LOGIN_SESSION_COOKIE')
 
 if (!loginSessionCookie && !loginSecureCookie) {
   throw new Error(
-    "Login cookies are not set, you must set one of the following environment variables: LOGIN_SESSION_COOKIE or LOGIN_SECURE_COOKIE"
+    'Login cookies are not set, you must set one of the following environment variables: LOGIN_SESSION_COOKIE or LOGIN_SECURE_COOKIE'
   )
 }
 
 // @ts-ignore
-Cypress.Commands.add("login", () => {
-  cy.session("mySession", () => {
+Cypress.Commands.add('login', () => {
+  cy.session('mySession', () => {
     // We need to refresh this cookie once in a while.
     // We are unsure if this is true and if true, when it needs to be refreshed.
     // console.log(loginSessionCookie)
     if (loginSessionCookie) {
-      cy.setCookie("next-auth.session-token", loginSessionCookie)
+      cy.setCookie('next-auth.session-token', loginSessionCookie)
     }
     if (loginSecureCookie) {
-      cy.setCookie("__Secure-next-auth.session-token", loginSecureCookie, {
+      cy.setCookie('__Secure-next-auth.session-token', loginSecureCookie, {
         secure: true,
       })
     }
